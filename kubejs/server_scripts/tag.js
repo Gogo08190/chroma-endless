2f7a481ec5f10210aba6392387b7feb5218d0c61
Gogo08 <gogo08190@gmail.com>
2022-05-14 08:25:00 +0200

    Init modpack

--- kubejs/server_scripts/tag.js
+++
@@ -1,6 +0,0 @@
-// priority: 100
-onEvent("item.tags", (event) => {
-    event.add("tconstruct:tables/station", /^tconstruct:tinker_station/g);
-    event.add("tconstruct:tables/builder", /^tconstruct:part_builder/g);
-    event.add("itemfilters:check_nbt", ["extendedcrafting:singularity", "patchouli:guide_book", "immersiveengineering:blueprint"]);
-});
