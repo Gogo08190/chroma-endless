3435e8871c3a55d8037eeec1448046c60d67eb87
Hugo <gogo08190@gmail.com>
2022-05-17 11:18:36 +0200

    pneumaticcraft add fuels + heat

---
+++ kubejs/server_scripts/chroma/kubejs/base/recipetypes/pneumaticcraft/fuels.js
@@ -0,0 +1,34 @@
+onEvent('recipes', (event) => {
+    var multiplier = 1000;
+    var data = {
+        recipes: [
+            {
+                fluid: 'mekanismgenerators:bioethanol',
+                air: 400,
+                rate: 1
+            },
+            {
+                fluid: 'industrialforegoing:biofuel',
+                air: 400,
+                rate: 1
+            },
+            {
+                fluid: 'immersiveengineering:creosote',
+                air: 50,
+                rate: 0.25
+            }
+        ]
+    };
+    data.recipes.forEach((recipe) => {
+        event.custom({
+            type: 'pneumaticcraft:fuel_quality',
+            fluid: {
+                type: 'pneumaticcraft:fluid',
+                fluid: recipe.fluid,
+                amount: 1000
+            },
+            air_per_bucket: recipe.air * multiplier,
+            burn_rate: recipe.rate
+        });
+    });
+});
