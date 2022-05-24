66696f4b7d2210dc4ba98227f081e35564cdb050
Z4RM <48418157+Z4RM@users.noreply.github.com>
2022-05-24 21:24:14 +0200

    Items for Welcome quests page

---
+++ kubejs/startup_scripts/items.js
@@ -0,0 +1,11 @@
+onEvent('item.registry', event => {
+
+    // Icônes Quêtes
+    event.create('challenges')
+    event.create('chunks')
+    event.create('discord')
+    event.create('sound_muffler')
+    event.create('teams')
+    event.create('website')
+
+})
