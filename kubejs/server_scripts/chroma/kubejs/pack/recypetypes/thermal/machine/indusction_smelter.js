b8de07c518c8c12c2dc64acd4fcd705e01f820b6
Hugo <gogo08190@gmail.com>
2022-05-25 09:58:01 +0200

    Fix

--- kubejs/server_scripts/chroma/kubejs/pack/recypetypes/thermal/machine/indusction_smelter.js
+++ kubejs/server_scripts/chroma/kubejs/pack/recypetypes/thermal/machine/indusction_smelter.js
@@ -2,12 +2,12 @@ onEvent('recipes', (event) => {
     const id_prefix = 'chroma:pack/thermal/induction_smelter';
     const recipes = [
         {
-            inputs: [Item.of('#forge:ingots/iron', 3), 'minecraft:andesite'],
+            inputs: [Item.of('#forge:ingots/iron', 1), 'minecraft:andesite'],
             outputs: [Item.of('create:andesite_alloy', 9)],
             id: `${id_prefix}andesite_alloy`
         },
         {
-            inputs: [Item.of('#forge:ingots/zinc', 3), 'minecraft:andesite'],
+            inputs: [Item.of('#forge:ingots/zinc', 1), 'minecraft:andesite'],
             outputs: [Item.of('create:andesite_alloy', 9)],
             id: `${id_prefix}andesite_alloy`
         }
