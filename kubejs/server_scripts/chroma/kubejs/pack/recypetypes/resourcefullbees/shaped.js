ecccd2acca7af6c39ce6bea5ae591963eb156d68
Gogo08 <gogo08190@gmail.com>
2022-05-30 17:44:34 +0200

    Fix wirecutter + fix beescript

--- kubejs/server_scripts/chroma/kubejs/pack/recypetypes/resourcefullbees/shaped.js
+++ kubejs/server_scripts/chroma/kubejs/pack/recypetypes/resourcefullbees/shaped.js
@@ -15,15 +15,13 @@ onEvent('recipes', (event) => {
     treeRegistry.forEach((treeCategories) => {
         if (treeCategories.type == 'tree') {
             treeCategories.trees.forEach((tree) => {
-                if (tree.trunk != 'minecraft:oak_log') {
-                    event
-                        .shaped(Item.of(tree.trunk, 8), ['WHW', 'HSH', 'WHW'], {
-                            S: tree.sapling,
-                            H: 'resourcefulbees:forest_honeycomb',
-                            W: 'resourcefulbees:wax'
-                        })
-                        .id(`${id_prefix}${tree.trunk.replace(':', '_')}_from_${tree.sapling.replace(':', '_')}`);
-                }
+              event
+                  .shaped(Item.of(tree.trunk, 8), ['WHW', 'HSH', 'WHW'], {
+                      S: tree.sapling,
+                      H: 'resourcefulbees:forest_honeycomb',
+                      W: 'resourcefulbees:wax'
+                  })
+                  .id(`${id_prefix}${tree.trunk.replace(':', '_')}_from_${tree.sapling.replace(':', '_')}`);
             });
         }
     });
