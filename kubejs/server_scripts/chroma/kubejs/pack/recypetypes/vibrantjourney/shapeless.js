7ef75fc4244af950b4b508f9c365c58994646c1d
Gogo08 <gogo08190@gmail.com>
2022-05-30 13:23:48 +0200

    Vibrants rocks

--- kubejs/server_scripts/chroma/kubejs/pack/recypetypes/vibrantjourney/shapeless.js
+++ kubejs/server_scripts/chroma/kubejs/pack/recypetypes/vibrantjourney/shapeless.js
@@ -14,6 +14,20 @@ onEvent('recipes', (event) => {
                 'minecraft:mossy_cobblestone'
             ],
             id: `${id_prefex}rocks_from_mossy`
+        },
+        {
+            output: Item.of('projectvibrantjourneys:sandstone_rocks', 4),
+            inputs: [
+                'minecraft:sandstone'
+            ],
+            id: `${id_prefex}rocks_from_sandstone`
+        },
+        {
+            output: Item.of('projectvibrantjourneys:red_sandstone_rocks', 4),
+            inputs: [
+                'minecraft:red_sandstone'
+            ],
+            id: `${id_prefex}rocks_from_red_sandstone`
         }
     ];
 
