cf8e60e5efc617fbe01f40f02ffdd3de236b8485
LerouxVictor <lerouxvictor3@gmail.com>
2022-05-30 23:05:16 +0200

    Added new recipes for Occultism mod :sparkles:

--- kubejs/server_scripts/chroma/kubejs/pack/recypetypes/occultism/shaped.js
+++ kubejs/server_scripts/chroma/kubejs/pack/recypetypes/occultism/shaped.js
@@ -12,7 +12,208 @@ onEvent('recipes', (event) => {
     */
 
     const recipes = [
+        {
+            output: 'occultism:otherstone_frame',
+            pattern: ['OOO', 'OAO', 'OOO'],
+            key: {
+                O: 'occultism:otherstone',
+                A: 'create:andesite_alloy'
+            },
+            id: 'occultism:otherstone_frame'
+        },
 
+        {
+            output: 'occultism:otherstone_tablet',
+            pattern: ['ASA', 'SSS', 'ASA'],
+            key: {
+                S: 'occultism:otherstone_slab',
+                A: 'create:andesite_alloy'
+            },
+            id: 'occultism:otherstone_tablet'
+        },
+
+        {
+            output: 'occultism:chalk_white_impure',
+            pattern: [' HB', 'HAH', 'BH '],
+            key: {
+                A: 'create:andesite_alloy',
+                H: 'occultism:otherworld_ashes',
+                B: 'occultism:burnt_otherstone'
+            },
+            id: 'occultism:chalk_white_impure'
+        },
+
+        {
+            output: 'occultism:otherstone_pedestal',
+            pattern: ['SGS', 'AOA', 'SSS'],
+            key: {
+                O: 'occultism:otherstone',
+                A: 'create:andesite_alloy',
+                G: 'occultism:spirit_attuned_gem',
+                S: 'occultism:otherstone_slab'
+            },
+            id: 'occultism:otherstone_pedestal'
+        },
+
+        {
+            output: 'occultism:spirit_attuned_pickaxe_head',
+            pattern: [' A ', 'GGG', 'A A'],
+            key: {
+                A: 'create:andesite_alloy',
+                G: 'occultism:spirit_attuned_gem'
+            },
+            id: 'occultism:spirit_attuned_pickaxe_head'
+        },
+
+        {
+            output: 'occultism:lenses',
+            pattern: ['CCC', 'CGC', 'CCC'],
+            key: {
+                C: 'tconstruct:clear_glass',
+                G: 'occultism:spirit_attuned_gem'
+            },
+            id: 'occultism:lenses'
+        },
+
+        {
+            output: 'occultism:lens_frame',
+            pattern: ['OOO', 'SBS', 'OOO'],
+            key: {
+                O: 'occultism:otherstone',
+                S: '#forge:ingots/silver',
+                B: '#forge:ingots/brass'
+            },
+            id: 'occultism:lens_frame'
+        },
+
+        {
+            output: 'occultism:magic_lamp_empty',
+            pattern: [' S ', 'SIS', ' SB'],
+            key: {
+                I: 'occultism:iesnium_ingot',
+                S: '#forge:ingots/silver',
+                B: '#forge:ingots/brass'
+            },
+            id: 'occultism:magic_lamp_empty'
+        },
+
+        {
+            output: 'occultism:divination_rod',
+            pattern: ['SGS', 'TAT', ' C '],
+            key: {
+                C: '#forge:rods/copper',
+                T: 'immersiveengineering:stick_treated',
+                A: 'create:andesite_alloy',
+                G: 'occultism:spirit_attuned_gem',
+                S: 'minecraft:stick'
+            },
+            id: 'occultism:divination_rod'
+        },
+
+        {
+            output: 'occultism:brush',
+            pattern: ['   ', 'BBB', 'WWC'],
+            key: {
+                B: 'create:brass_casing',
+                W: 'dustrial_decor:mini_padded_block',
+                C: 'comforts:hammock_white'
+            },
+            id: 'occultism:brush'
+        },
+
+        {
+            output: Item.of('occultism:butcher_knife', '{Damage:0}'),
+            pattern: [' AI', 'AI ', 'S  '],
+            key: {
+                S: 'minecraft:stick',
+                I: '#forge:storage_blocks/iron',
+                A: 'create:andesite_alloy'
+            },
+            id: Item.of('occultism:butcher_knife', '{Damage:0}'),
+        },
+
+        {
+            output: Item.of('occultism:iesnium_pickaxe', '{Damage:0}'),
+            pattern: ['IDI', ' C ', ' S '],
+            key: {
+                S: 'minecraft:stick',
+                C: '#forge:rods/copper',
+                D: 'create:shadow_steel',
+                I: 'occultism:iesnium_ingot'
+            },
+            id: Item.of('occultism:iesnium_pickaxe', '{Damage:0}')
+        },
+
+        {
+            output: 'occultism:golden_sacrificial_bowl',
+            pattern: ['GBG', 'BSB', 'GBG'],
+            key: {
+                S: 'occultism:sacrificial_bowl',
+                B: '#forge:ingots/brass',
+                G: '#forge:ingots/gold'
+            },
+            id: 'occultism:golden_sacrificial_bowl'
+        },
+
+        {
+            output: 'occultism:book_of_binding_foliot',
+            pattern: ['BDB', 'AQA', 'BDB'],
+            key: {
+                A: 'create:andesite_alloy',
+                Q: 'minecraft:writable_book',
+                D: '#forge:dyes/blue',
+                B: 'createdeco:blue_brick'
+            },
+            id: 'occultism:book_of_binding_foliot'
+        },
+
+        {
+            output: 'occultism:book_of_binding_djinni',
+            pattern: ['SBS', 'AQA', 'SBS'],
+            key: {
+                A: 'create:andesite_alloy',
+                Q: 'minecraft:writable_book',
+                S: 'createdeco:scarlet_brick',
+                B: 'createdeco:blue_brick'
+            },
+            id: 'occultism:book_of_binding_djinni'
+        },
+
+        {
+            output: 'occultism:book_of_binding_afrit',
+            pattern: ['DYD', 'AQA', 'DYD'],
+            key: {
+                A: 'create:andesite_alloy',
+                Q: 'minecraft:writable_book',
+                Y: '#forge:dyes/yellow',
+                D: 'createdeco:dean_brick'
+            },
+            id: 'occultism:book_of_binding_afrit'
+        },
+
+        {
+            output: 'occultism:book_of_binding_marid',
+            pattern: ['DBD', 'AQA', 'DBD'],
+            key: {
+                A: 'create:andesite_alloy',
+                Q: 'minecraft:writable_book',
+                B: '#forge:dyes/blue',
+                D: 'createdeco:dean_brick'
+            },
+            id: 'occultism:book_of_binding_marid'
+        },
+
+        {
+            output: 'occultism:storage_controller',
+            pattern: ['ADA', 'BCB', 'ABA'],
+            key: {
+                A: 'create:andesite_alloy',
+                C: 'occultism:storage_controller_base',
+                B: '#forge:ingots/brass',
+                D: '#occultism:dimensional_matrix'
+            },
+            id: 'occultism:storage_controller'
+        }
     ];
 
     recipes.forEach((recipe) => {
