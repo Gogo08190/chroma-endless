d88959ff50258c85c35c84b0d7ad8c16c7c04a89
Hugo <gogo08190@gmail.com>
2022-05-31 08:32:46 +0200

    init summon ritual file

---
+++ kubejs/server_scripts/chroma/kubejs/pack/recypetypes/occultism/ritual.js
@@ -0,0 +1,50 @@
+onEvent('recipes', (event) => {
+    const id_prefix = 'chroma:base/occultism/ritual/';
+    const recipes = [
+        {
+            ritual_type: 'occultism:summon',
+            activation_item: 'occultism:book_of_binding_bound_foliot',
+            duration: 120,
+            entity_to_sacrifice: {
+                tag: 'mob a sacrifer',
+                display_name: 'nom a afficher'
+            },
+            pentacle_id: 'id du pentacle',
+            item_to_use: 'item a utiliser',
+            entity_to_summon: 'entité a summon',
+            ritual_dummy: 'logo du rituel',
+            ingredients: [
+                '12 o'clock' ,
+                '3 o'clock' ,
+                '6 o'clock' ,
+                '9 o'clock' ,
+
+                '1 o'clock',
+                '2 o'clock' ,
+                '7 o'clock' ,
+                '8 o'clock' ,
+
+                '11 o'clock' ,
+                '4 o'clock' ,
+                '5 o'clock' ,
+                '10 o'clock',
+            ],
+            result: 'occultism:jei_dummy/none',
+            id: `${id_prefix}summon_nom`
+        }
+    ];
+
+    recipes.forEach((recipe) => {
+        recipe.type = 'occultism:ritual';
+
+        recipe.activation_item = Ingredient.of(recipe.activation_item).toJson();
+        if (recipe.item_to_use) {
+            recipe.item_to_use = Ingredient.of(recipe.item_to_use).toJson();
+        }
+        recipe.ritual_dummy = Ingredient.of(recipe.ritual_dummy).toJson();
+        recipe.ingredients = recipe.ingredients.map((input) => Ingredient.of(input).toJson());
+        recipe.result = Item.of(recipe.result).toJson();
+
+        event.custom(recipe).id(recipe.id);
+    });
+});
