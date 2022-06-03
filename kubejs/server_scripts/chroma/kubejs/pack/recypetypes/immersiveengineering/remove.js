08914f586afcdb613f8619b17046d362be69d90a
Neolio_ <neolio.ytb@gmail.com>
2022-06-03 10:57:53 +0200

    Remove Immersive craft for all Sheetmetal

--- kubejs/server_scripts/chroma/kubejs/pack/recypetypes/immersiveengineering/remove.js
+++ kubejs/server_scripts/chroma/kubejs/pack/recypetypes/immersiveengineering/remove.js
@@ -5,7 +5,18 @@ onEvent('recipes', (event) => {
       'immersiveengineering:crafting/alloybrick',
       'immersiveengineering:crafting/blastbrick',
       'immersiveengineering:crafting/cokebrick',
-      'immersiveengineering:crafting/blastbrick_reinforced'
+      'immersiveengineering:crafting/blastbrick_reinforced',
+      'immersiveengineering:crafting/sheetmetal_iron',
+      'immersiveengineering:crafting/sheetmetal_steel',
+      'immersiveengineering:crafting/sheetmetal_nickel',
+      'immersiveengineering:crafting/sheetmetal_lead',
+      'immersiveengineering:crafting/sheetmetal_aluminum',
+      'immersiveengineering:crafting/sheetmetal_silver',
+      'immersiveengineering:crafting/sheetmetal_gold',
+      'immersiveengineering:crafting/sheetmetal_copper',
+      'immersiveengineering:crafting/sheetmetal_electrum',
+      'immersiveengineering:crafting/sheetmetal_constantan',
+      'immersiveengineering:crafting/sheetmetal_uranium'
     ];
 
     const outputRemovals = [
