7aa4b2028463d5cdf641cbef95bd6eaf369553bf
Neolio_ <neolio.ytb@gmail.com>
2022-06-03 10:46:00 +0200

    Add Compacting all Sheetmetal from Immersive

--- kubejs/server_scripts/chroma/kubejs/pack/recypetypes/create/compacting.js
+++ kubejs/server_scripts/chroma/kubejs/pack/recypetypes/create/compacting.js
@@ -12,6 +12,83 @@ onEvent('recipes', (event) => {
             output: Item.of('immersiveengineering:blastbrick_reinforced', 1),
             type: 'superheated',
             id: `${id_prefix}blastbrick_reinforced`
+        },
+        //Sheetmetal Iron
+        {
+            inputs: [Item.of('#forge:plates/iron', 4)],
+            output: Item.of('immersiveengineering:sheetmetal_iron', 1),
+            type: 'unheated',
+            id: `${id_prefix}sheetmetal_iron`
+        },
+        //Sheetmetal Steel
+        {
+            inputs: [Item.of('#forge:plates/steel', 4)],
+            output: Item.of('immersiveengineering:sheetmetal_steel', 1),
+            type: 'unheated',
+            id: `${id_prefix}sheetmetal_steel`
+        },
+        //Sheetmetal Nickel
+        {
+            inputs: [Item.of('#forge:plates/nickel', 4)],
+            output: Item.of('immersiveengineering:sheetmetal_nickel', 1),
+            type: 'unheated',
+            id: `${id_prefix}sheetmetal_nickel`
+        },
+        //Sheetmetal Lead
+        {
+            inputs: [Item.of('#forge:plates/lead', 4)],
+            output: Item.of('immersiveengineering:sheetmetal_lead', 1),
+            type: 'unheated',
+            id: `${id_prefix}sheetmetal_lead`
+        },
+        //Sheetmetal Aluminum
+        {
+            inputs: [Item.of('#forge:plates/aluminum', 4)],
+            output: Item.of('immersiveengineering:sheetmetal_aluminum', 1),
+            type: 'unheated',
+            id: `${id_prefix}sheetmetal_aluminum`
+        },
+        //Sheetmetal Silver
+        {
+            inputs: [Item.of('#forge:plates/silver', 4)],
+            output: Item.of('immersiveengineering:sheetmetal_silver', 1),
+            type: 'unheated',
+            id: `${id_prefix}sheetmetal_silver`
+        },
+        //Sheetmetal Gold
+        {
+            inputs: [Item.of('#forge:plates/gold', 4)],
+            output: Item.of('immersiveengineering:sheetmetal_gold', 1),
+            type: 'unheated',
+            id: `${id_prefix}sheetmetal_gold`
+        },
+        //Sheetmetal Copper
+        {
+            inputs: [Item.of('#forge:plates/copper', 4)],
+            output: Item.of('immersiveengineering:sheetmetal_copper', 1),
+            type: 'unheated',
+            id: `${id_prefix}sheetmetal_copper`
+        },
+        //Sheetmetal Electrum
+        {
+            inputs: [Item.of('#forge:plates/electrum', 4)],
+            output: Item.of('immersiveengineering:sheetmetal_electrum', 1),
+            type: 'unheated',
+            id: `${id_prefix}sheetmetal_electrum`
+        },
+        //Sheetmetal Constantan
+        {
+            inputs: [Item.of('#forge:plates/constantan', 4)],
+            output: Item.of('immersiveengineering:sheetmetal_constantan', 1),
+            type: 'unheated',
+            id: `${id_prefix}sheetmetal_constantan`
+        },
+        //Sheetmetal Uranium
+        {
+            inputs: [Item.of('#forge:plates/uranium', 4)],
+            output: Item.of('immersiveengineering:sheetmetal_uranium', 1),
+            type: 'unheated',
+            id: `${id_prefix}sheetmetal_uranium`
         }
     ];
 
