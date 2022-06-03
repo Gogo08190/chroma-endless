19bc25e10c780adb622eb389f2673a936f89b5ab
Gogo08 <gogo08190@gmail.com>
2022-06-03 13:22:15 +0200

    fix Vibrant recipes

--- kubejs/server_scripts/chroma/kubejs/pack/recypetypes/vibrantjourney/shapeless.js
+++ kubejs/server_scripts/chroma/kubejs/pack/recypetypes/vibrantjourney/shapeless.js
@@ -1,33 +1,33 @@
 onEvent('recipes', (event) => {
-    const id_prefex = 'chroma:pack/';
+    const id_prefix = 'chroma:pack/';
     const recipes = [
         {
             output: Item.of('projectvibrantjourneys:rocks', 4),
             inputs: [
                 'minecraft:cobblestone'
             ],
-            id: `${id_prefex}rocks_from_cobble`
+            id: `${id_prefix}rocks_from_cobble`
         },
         {
             output: Item.of('projectvibrantjourneys:mossy_rocks', 4),
             inputs: [
                 'minecraft:mossy_cobblestone'
             ],
-            id: `${id_prefex}rocks_from_mossy`
+            id: `${id_prefix}rocks_from_mossy`
         },
         {
             output: Item.of('projectvibrantjourneys:sandstone_rocks', 4),
             inputs: [
                 'minecraft:sandstone'
             ],
-            id: `${id_prefex}rocks_from_sandstone`
+            id: `${id_prefix}rocks_from_sandstone`
         },
         {
             output: Item.of('projectvibrantjourneys:red_sandstone_rocks', 4),
             inputs: [
                 'minecraft:red_sandstone'
             ],
-            id: `${id_prefex}rocks_from_red_sandstone`
+            id: `${id_prefix}rocks_from_red_sandstone`
         }
     ];
 
