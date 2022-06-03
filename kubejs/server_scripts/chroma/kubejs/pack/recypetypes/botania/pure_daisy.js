e44499399feb63a078a3e84d0975b012e43dba41
Hugo <gogo08190@gmail.com>
2022-06-03 08:20:52 +0200

    Init pure daisy recipes

---
+++ kubejs/server_scripts/chroma/kubejs/pack/recypetypes/botania/pure_daisy.js
@@ -0,0 +1,27 @@
+onEvent('recipes', (event) => {
+    /*{
+        input: '',
+        output: '',
+        id: ''
+    }*/
+
+    const recipes = [
+
+    ];
+
+    recipes.forEach((recipe) => {
+        let ingredient =
+            recipe.input.charAt(0) == '#'
+                ? { type: 'tag', tag: recipe.input.slice(1) }
+                : { type: 'block', block: recipe.input };
+
+        const re = event.custom({
+            type: 'botania:pure_daisy',
+            input: ingredient,
+            output: { name: recipe.output }
+        });
+        if (recipe.id) {
+            re.id(recipe.id);
+        }
+    });
+});
