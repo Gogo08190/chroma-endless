f40499cb1d24fa14df491ebb269c1698c546d67d
Thebloodshadow <45766546+Thebloodshadow@users.noreply.github.com>
2022-06-06 16:26:38 +0200

    add : Test recipe for explosion crafting

--- kubejs/server_scripts/chroma/kubejs/pack/recypetypes/pneumaticcraft/explosion_crafting.js
+++ kubejs/server_scripts/chroma/kubejs/pack/recypetypes/pneumaticcraft/explosion_crafting.js
@@ -2,23 +2,27 @@ onEvent('recipes', (event) => {
     const id_prefix = 'chroma:pack/pneumaticcraft/assembly/';
 
     /*{
-        input: item ,
-        output: item,
-        poucentage_lost: ,
+        input: { item: ''},
+        results: { item: '', count: nb },
+        loss_rate: nb,
         id: ``
     }*/
 
     const recipes = [
-
+      input: {item : 'undergarden:cloggrum_ingot'} ,
+      results: {item : 'pneumaticcraft:ingot_iron_compressed,',count : 1},
+      loss_rate: 10,
+      id: `pneumaticcraft:explosion_crafting/compressed_iron_ingot`
+  }
     ];
 
     recipes.forEach((recipe) => {
         event
             .custom({
                 type: `pneumaticcraft:explosion_crafting`,
-                pourcentage_lost: recipe.pourcentage_lost,
+                loss_rate: recipe.loss_rate,
                 input: recipe.input,
-                output: recipe.output,
+                results: recipe.results,
             })
             .id(recipe.id);
     });
