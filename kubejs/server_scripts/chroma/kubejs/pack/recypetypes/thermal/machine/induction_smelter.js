1972c100d6eb2b509af4ff692da91d420fbc3962
Gogo08 <gogo08190@gmail.com>
2022-06-06 17:50:05 +0200

    Ender / Redstone Ingot

--- kubejs/server_scripts/chroma/kubejs/pack/recypetypes/thermal/machine/induction_smelter.js
+++ kubejs/server_scripts/chroma/kubejs/pack/recypetypes/thermal/machine/induction_smelter.js
@@ -1,7 +1,16 @@
 onEvent('recipes', (event) => {
     const id_prefix = 'chroma:pack/thermal/induction_smelter';
     const recipes = [
-
+      {
+          inputs: ['#forge:ingots/iron', '#forge:dusts/redstone'],
+          outputs: ['extendedcrafting:redstone_ingot'],
+          id: `${id_prefix}redstone_ingot`
+      },
+      {
+          inputs: ['#forge:ingots/iron', 'minecraft:ender_pearl'],
+          outputs: ['extendedcrafting:ender_ingot'],
+          id: `${id_prefix}ender_ingot`
+      }
     ];
 
     recipes.forEach((recipe) => {
