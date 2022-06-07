37c91e606e1918af90b3fe936b8972b7b8fdcdcd
YukiiSamaa <thomas.levasseur.tl@gmail.com>
2022-06-07 20:28:17 +0200

    Add craft Linking,Upgrade tool

--- kubejs/server_scripts/chroma/kubejs/pack/recypetypes/minecraft/shaped.js
+++ kubejs/server_scripts/chroma/kubejs/pack/recypetypes/minecraft/shaped.js
@@ -16,6 +16,32 @@ onEvent('recipes', (event) => {
                 A: '#minecraft:logs'
             },
             id: `${id_prefex}sticks_16`
+        },
+        // Linking tool
+        {
+          output: 'pedestals:linkingtool',
+          pattern: [' AB',' CD','E  '],
+          key: {
+              A: 'minecraft:red_dye',
+              B: 'minecraft:green_dye',
+              C: 'betterendforge:leather_wrapped_stick',
+              D: 'minecraft:blue_dye',
+              E: 'botania:mana_diamond',
+          },
+          id: 'pedestals:linkingtool'
+        },
+        // Upgrade tool
+        {
+          output: 'pedestals:upgradetool',
+          pattern: [' AB',' CD','E  '],
+          key: {
+              A: 'minecraft:red_dye',
+              B: 'minecraft:green_dye',
+              C: 'betterendforge:leather_wrapped_stick',
+              D: 'minecraft:blue_dye',
+              E: 'emendatusenigmatica:lumium_plate',
+          },
+          id: 'pedestals:upgradetool'
         }
     ];
 
