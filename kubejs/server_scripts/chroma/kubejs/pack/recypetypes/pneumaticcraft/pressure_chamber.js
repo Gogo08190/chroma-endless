ebd3d627737ef4cda9c1b109c274cb273899934a
Gogo08 <gogo08190@gmail.com>
2022-06-08 18:25:19 +0200

    Fix recipes

--- kubejs/server_scripts/chroma/kubejs/pack/recypetypes/pneumaticcraft/pressure_chamber.js
+++ kubejs/server_scripts/chroma/kubejs/pack/recypetypes/pneumaticcraft/pressure_chamber.js
@@ -1,86 +1,67 @@
 onEvent('recipes', (event) => {
     const id_prefix = 'chroma:pack/pneumaticcraft/pressure_chamber/';
 
-    /*{
-        ingredients: [
-          { type: 'pneumaticcraft:stacked_item', item: '', count: nb } (Si item)
-          { type: 'pneumaticcraft:stacked_item', tag: '', count: nb } (Si tag)
-        ],
-        pressure: 2.0,
-        output: [
-          { item: '', count: nb }
-        ],
-        id: ''
-    }*/
-
     const recipes = [
         {
-            ingredients: [
-              { type: 'pneumaticcraft:stacked_item', item: 'undergarden:cloggrum_ingot', count: 1 }
+            inputs: [
+                { item: 'undergarden:cloggrum_ingot', count: 1 }
             ],
             pressure: 2.0,
-            output: [
-              { item: 'pneumaticcraft:ingot_iron_compressed', count: 1 }
-            ],
+            results: [{ item: 'pneumaticcraft:ingot_iron_compressed', count: 1 }],
             id: 'pneumaticcraft:pressure_chamber/compressed_iron_ingot'
         },
         {
-            ingredients: [
-              { type: 'pneumaticcraft:stacked_item', item: 'undergarden:cloggrum_block', count: 1 }
+            inputs: [
+                { item: 'undergarden:cloggrum_block', count: 1 }
             ],
             pressure: 2.0,
-            output: [
-              { item: 'pneumaticcraft:compressed_iron_block', count: 1 }
-            ],
+            results: [{ item: 'pneumaticcraft:compressed_iron_block', count: 1 }],
             id: 'pneumaticcraft:pressure_chamber/compressed_iron_block'
         },
         {
-            ingredients: [
-              { type: 'pneumaticcraft:stacked_item', item: 'pneumaticcraft:plastic', count: 1 },
-              { type: 'pneumaticcraft:stacked_item', tag: '#forge:nuggets/electrum', count: 3 },
-              { type: 'pneumaticcraft:stacked_item', item: 'thermal:redstone_servo', count: 2 }
+            inputs: [
+                { item: 'pneumaticcraft:plastic', count: 1 },
+                { item: 'extendedcrafting:redstone_ingot', count: 1 },
+                { tag: 'forge:nuggets/electrum', count: 3 }
             ],
-            pressure: 1.5,
-            output: [
-              { item: 'pneumaticcraft:empty_pcb', count: 3 }
-
-            ],
-            id: 'pneumaticcraft:pressure_chamber/empty_pcb'
+            pressure: 1.0,
+            results: [{ item: 'pneumaticcraft:transistor', count: 1 }],
+            id: 'pneumaticcraft:pressure_chamber/transistor'
         },
         {
-            ingredients: [
-              { type: 'pneumaticcraft:stacked_item', item: 'pneumaticcraft:plastic', count: 1 },
-              { type: 'pneumaticcraft:stacked_item', tag: '#forge:nuggets/electrum', count: 2 },
-              { type: 'pneumaticcraft:stacked_item', item: 'botania:mana_pearl', count: 1 }
+            inputs: [
+                { item: 'pneumaticcraft:plastic', count: 1 },
+                { item: 'botania:mana_pearl', count: 2 },
+                { tag: 'forge:nuggets/electrum', count: 1 }
             ],
             pressure: 1.5,
-            output: [
-              { item: 'pneumaticcraft:capacitor', count: 1 }
-
-            ],
+            results: [{ item: 'pneumaticcraft:capacitor', count: 1 }],
             id: 'pneumaticcraft:pressure_chamber/capacitor'
         },
         {
-            ingredients: [
-              { type: 'pneumaticcraft:stacked_item', item: 'pneumaticcraft:plastic', count: 1 },
-              { type: 'pneumaticcraft:stacked_item', tag: '#forge:nuggets/electrum', count: 2 },
-              { type: 'pneumaticcraft:stacked_item', item: 'extendedcrafting:redstone_ingot', count: 1 }
+            inputs: [
+                { item: 'pneumaticcraft:plastic', count: 1 },
+                { item: 'thermal:redstone_servo', count: 2 },
+                { tag: 'forge:nuggets/electrum', count: 3 }
             ],
             pressure: 1.5,
-            output: [
-              { item: 'pneumaticcraft:transistor', count: 1 }
-
-            ],
-            id: 'pneumaticcraft:pressure_chamber/transistor'
-        }
+            results: [{ item: 'pneumaticcraft:empty_pcb', count: 3 }],
+            id: 'pneumaticcraft:pressure_chamber/empty_pcb'
+        },
     ];
     recipes.forEach((recipe) => {
+        let ingredients = [];
+        recipe.inputs.forEach((input) => {
+            input.type = 'pneumaticcraft:stacked_item';
+            ingredients.push(input);
+        });
+
         event
             .custom({
                 type: 'pneumaticcraft:pressure_chamber',
-                inputs: recipe.ingredients,
+                inputs: ingredients,
                 pressure: recipe.pressure,
-                results: recipe.output
+                results: recipe.results
             })
             .id(recipe.id);
     });
