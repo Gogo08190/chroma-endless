614c2f563205ba6283d4f44543f5d8c2536603f1
Gogo08 <gogo08190@gmail.com>
2022-06-08 13:30:43 +0200

    Pneumaticcraft recipes

--- kubejs/server_scripts/chroma/kubejs/pack/recypetypes/pneumaticcraft/shaped.js
+++ kubejs/server_scripts/chroma/kubejs/pack/recypetypes/pneumaticcraft/shaped.js
@@ -11,6 +11,142 @@ onEvent('recipes', (event) => {
           },
           id: 'pneumaticcraft:air_canister'
       },
+      {
+          output: 'pneumaticcraft:reinforced_air_canister',
+          pattern: [' P ', 'CAC', 'CCC'],
+          key: {
+              P: 'pneumaticcraft:advanced_pressure_tube',
+              A: 'pneumaticcraft:air_canister',
+              C: 'pneumaticcraft:compressed_iron_block'
+          },
+          id: 'pneumaticcraft:reinforced_air_canister'
+      },
+      {
+          output: Item.of('pneumaticcraft:reinforced_stone', 8),
+          pattern: ['CSC', 'SCS', 'CSC'],
+          key: {
+              S: 'undergarden:shiverstone',
+              C: '#forge:ingots/compressed_iron'
+          },
+          id: 'pneumaticcraft:reinforced_stone'
+      },
+      {
+          output: Item.of('pneumaticcraft:pressure_tube', 8),
+          pattern: ['CGC'],
+          key: {
+              G: 'thermal:obsidian_glass',
+              C: '#forge:ingots/compressed_iron'
+          },
+          id: 'pneumaticcraft:pressure_tube'
+      },
+      {
+          output: 'pneumaticcraft:small_tank',
+          pattern: ['BCB', 'CGC', 'BCB'],
+          key: {
+            G: 'thermal:obsidian_glass',
+            C: '#forge:ingots/compressed_iron',
+            B: 'undergarden:cloggrum_bars'
+          },
+          id: 'pneumaticcraft:small_tank'
+      },
+      {
+          output: 'pneumaticcraft:heat_sink',
+          pattern: ['BBB', 'CGC'],
+          key: {
+            G: '#forge:ingots/brass',
+            C: '#forge:ingots/compressed_iron',
+            B: 'undergarden:cloggrum_bars'
+          },
+          id: 'pneumaticcraft:heat_sink'
+      },
+      {
+          output: 'pneumaticcraft:vortex_tube',
+          pattern: ['CTC', 'GTG', 'CCC'],
+          key: {
+            G: '#forge:ingots/brass',
+            C: '#forge:ingots/compressed_iron',
+            T: 'pneumaticcraft:pressure_tube'
+          },
+          id: 'pneumaticcraft:vortex_tube'
+      },
+      {
+          output: 'pneumaticcraft:refinery',
+          pattern: ['SSS', 'RTR', 'SSS'],
+          key: {
+            S: 'pneumaticcraft:reinforced_stone_slab',
+            R: 'extendedcrafting:redstone_ingot',
+            T: 'pneumaticcraft:small_tank'
+          },
+          id: 'pneumaticcraft:refinery'
+      },
+      {
+          output: 'pneumaticcraft:refinery_output',
+          pattern: ['SSS', 'RTR', 'SSS'],
+          key: {
+            S: 'pneumaticcraft:reinforced_stone_slab',
+            R: 'thermal:obsidian_glass',
+            T: '#forge:gems/mana_diamond'
+          },
+          id: 'pneumaticcraft:refinery_output'
+      },
+      {
+          output: 'pneumaticcraft:thermopneumatic_processing_plant',
+          pattern: ['SCS', 'RTR', 'SCS'],
+          key: {
+            S: 'pneumaticcraft:reinforced_stone_slab',
+            R: 'pneumaticcraft:small_tank',
+            T: 'pneumaticcraft:pressure_tube',
+            C: '#forge:ingots/cloggrum'
+          },
+          id: 'pneumaticcraft:thermopneumatic_processing_plant'
+      },
+      {
+          output: 'pneumaticcraft:air_compressor',
+          pattern: ['UUU', 'SAT', 'SFS'],
+          key: {
+            S: 'pneumaticcraft:reinforced_bricks',
+            F: 'minecraft:furnace',
+            A: 'pneumaticcraft:air_canister',
+            T: 'pneumaticcraft:pressure_tube',
+            U: '#forge:ingots/utherium'
+          },
+          id: 'pneumaticcraft:air_compressor'
+      },
+      {
+          output: 'pneumaticcraft:liquid_compressor',
+          pattern: ['UUU', 'TAT', 'CPC'],
+          key: {
+            T: 'pneumaticcraft:pressure_tube',
+            U: '#forge:ingots/utherium',
+            C: 'thermal:cured_rubber',
+            P: 'pneumaticcraft:air_compressor',
+            A: 'pneumaticcraft:small_tank'
+          },
+          id: 'pneumaticcraft:liquid_compressor'
+      },
+      {
+          output: 'pneumaticcraft:charging_station',
+          pattern: ['  T', 'BUB', 'SSS'],
+          key: {
+            T: 'pneumaticcraft:pressure_tube',
+            U: '#forge:ingots/utherium',
+            B: '#forge:ingots/brick',
+            S: 'pneumaticcraft:reinforced_stone_slab'
+          },
+          id: 'pneumaticcraft:charging_station'
+      },
+      {
+          output: 'pneumaticcraft:uv_light_box',
+          pattern: ['GLG', 'CPT', 'CCC'],
+          key: {
+            G: 'thermal:signalum_glass',
+            L: 'simplylight:illuminant_red_block',
+            C: '#forge:ingots/compressed_iron',
+            P: 'pneumaticcraft:pcb_blueprint',
+            T: 'pneumaticcraft:pressure_tube'
+          },
+          id: 'pneumaticcraft:uv_light_box'
+      }
     ];
 
     recipes.forEach((recipe) => {
