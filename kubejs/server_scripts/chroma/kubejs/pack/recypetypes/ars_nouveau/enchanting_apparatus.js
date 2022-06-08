02b05e460d4e8507505b35f5039426845b0d2a96
YukiiSamaa <thomas.levasseur.tl@gmail.com>
2022-06-08 18:35:17 +0200

    Fix shaped + Pedestal enchanting apparatus

--- kubejs/server_scripts/chroma/kubejs/pack/recypetypes/ars_nouveau/enchanting_apparatus.js
+++ kubejs/server_scripts/chroma/kubejs/pack/recypetypes/ars_nouveau/enchanting_apparatus.js
@@ -20,6 +20,59 @@ onEvent('recipes', (event) => {
 
     const recipes = [
 
+      // Pedestal Upgrade Base
+      {
+          inputs: [
+              'emendatusenigmatica:lumium_plate',
+              'emendatusenigmatica:lumium_plate',
+              'emendatusenigmatica:lumium_plate',
+              'emendatusenigmatica:lumium_plate',
+              '',
+              '',
+              '',
+              ''
+          ],
+          reagent: 'emendatusenigmatica:lumium_plate',
+          sourceCost: 5000,
+          output: 'pedestals:coin/default',
+          id: 'pedestals:coin/default'
+      },
+
+      // Pedestal Upgrade Base
+      {
+          inputs: [
+              Item.of('elementalcraft:element_holder_water', '{Damage:0,element_amount:10000}'),
+              Item.of('elementalcraft:element_holder_fire', '{Damage:0,element_amount:10000}'),
+              Item.of('elementalcraft:element_holder_water', '{Damage:0,element_amount:10000}'),
+              Item.of('elementalcraft:element_holder_fire', '{Damage:0,element_amount:10000}'),
+              Item.of('elementalcraft:element_holder_fire', '{Damage:0,element_amount:10000}'),
+              Item.of('elementalcraft:element_holder_water', '{Damage:0,element_amount:10000}'),
+              Item.of('elementalcraft:element_holder_fire', '{Damage:0,element_amount:10000}'),
+              Item.of('elementalcraft:element_holder_water', '{Damage:0,element_amount:10000}')
+          ],
+          reagent: 'pedestals:coin/default',
+          sourceCost: 5000,
+          output: 'pedestals:coin/cobble',
+          id: 'pedestals:coin/cobble'
+      },
+
+      // Pedestal
+      {
+          inputs: [
+            'occultism:otherstone_slab',
+            'pedestals:linkingtool',
+            'occultism:otherstone_slab',
+            '',
+            '',
+            'occultism:otherstone',
+            'occultism:otherstone',
+            'occultism:otherstone'
+          ],
+          reagent: 'occultism:otherstone',
+          sourceCost: 5000,
+          output: 'pedestals:pedestal/stone000',
+          id: 'pedestals:pedestal/stone000'
+      }
     ];
 
     recipes.forEach((recipe) => {
