955d4c303d02f6e603297fba4fabd1bc29005103
koepss <pierrelode2@outlook.fr>
2022-06-09 18:20:42 +0200

    change recipe elementium armor

--- kubejs/server_scripts/chroma/kubejs/pack/recypetypes/botania/elven_trade.js
+++ kubejs/server_scripts/chroma/kubejs/pack/recypetypes/botania/elven_trade.js
@@ -19,6 +19,70 @@ onEvent('recipes', (event) => {
             item: 'elementalcraft:containedcrystal'
         },
         id: 'elementalcraft:containedcrystal'
+      },
+
+      {
+        inputs: [
+          {item: Item.of('botania:manasteel_helmet', '{Damage:0}').weakNBT()},
+          {item: 'botania:elementium_ingot'},
+          {item: 'botania:elementium_ingot'},
+          {item: 'botania:elementium_ingot'},
+          {item: 'botania:elementium_ingot'},
+          {item: 'botania:elementium_ingot'}
+        ],
+        output: {
+            item: Item.of('botania:elementium_helmet', '{Damage:0}').weakNBT()
+        },
+        id: 'botania:elementium_helmet'
+      },
+
+      {
+        inputs: [
+          {item: Item.of('botania:manasteel_chestplate', '{Damage:0}').weakNBT()},
+          {item: 'botania:elementium_ingot'},
+          {item: 'botania:elementium_ingot'},
+          {item: 'botania:elementium_ingot'},
+          {item: 'botania:elementium_ingot'},
+          {item: 'botania:elementium_ingot'},
+          {item: 'botania:elementium_ingot'},
+          {item: 'botania:elementium_ingot'},
+          {item: 'botania:elementium_ingot'}
+        ],
+        output: {
+            item: Item.of('botania:elementium_chestplate', '{Damage:0}').weakNBT()
+        },
+        id: 'botania:elementium_chestplate'
+      },
+
+      {
+        inputs: [
+          {item: Item.of('botania:manasteel_leggings', '{Damage:0}').weakNBT()},
+          {item: 'botania:elementium_ingot'},
+          {item: 'botania:elementium_ingot'},
+          {item: 'botania:elementium_ingot'},
+          {item: 'botania:elementium_ingot'},
+          {item: 'botania:elementium_ingot'},
+          {item: 'botania:elementium_ingot'},
+          {item: 'botania:elementium_ingot'}
+        ],
+        output: {
+            item: Item.of('botania:elementium_leggings', '{Damage:0}').weakNBT()
+        },
+        id: 'botania:elementium_leggings'
+      },
+
+      {
+        inputs: [
+          {item: Item.of('botania:manasteel_boots', '{Damage:0}').weakNBT()},
+          {item: 'botania:elementium_ingot'},
+          {item: 'botania:elementium_ingot'},
+          {item: 'botania:elementium_ingot'},
+          {item: 'botania:elementium_ingot'}
+        ],
+        output: {
+            item: Item.of('botania:elementium_boots', '{Damage:0}').weakNBT()
+        },
+        id: 'botania:elementium_boots'
       }
 
     ];
