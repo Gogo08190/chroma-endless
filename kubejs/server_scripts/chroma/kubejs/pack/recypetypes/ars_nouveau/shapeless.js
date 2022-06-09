1e5083f8622caa00c4fd4cb82e2cd85afa593105
Gogo08 <gogo08190@gmail.com>
2022-06-09 21:39:56 +0200

    Starting Rftools storage + init masterfull configs

--- kubejs/server_scripts/chroma/kubejs/pack/recypetypes/ars_nouveau/shapeless.js
+++
@@ -1,9 +0,0 @@
-onEvent('recipes', (event) => {
-    const recipes = [
-      
-    ];
-
-    recipes.forEach((recipe) => {
-        event.shapeless(recipe.output, recipe.inputs).id(recipe.id);
-    });
-});
