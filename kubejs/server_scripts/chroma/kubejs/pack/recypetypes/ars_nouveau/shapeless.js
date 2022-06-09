14cd3bbb4103758d6efdec7ed57b43dbda6eb9bb
LerouxVictor <lerouxvictor3@gmail.com>
2022-06-09 20:17:07 +0200

    Init shapeless Ars Nouveau :sparkles:

---
+++ kubejs/server_scripts/chroma/kubejs/pack/recypetypes/ars_nouveau/shapeless.js
@@ -0,0 +1,9 @@
+onEvent('recipes', (event) => {
+    const recipes = [
+      
+    ];
+
+    recipes.forEach((recipe) => {
+        event.shapeless(recipe.output, recipe.inputs).id(recipe.id);
+    });
+});
