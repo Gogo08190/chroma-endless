a8b809700dd8ea366634a55a089ea1a20d5f1508
mathieucrombe <mathieucrombe@gmail.com>
2022-06-10 22:08:06 +0200

    start ading chipset

--- kubejs/startup_scripts/item_registry.js
+++ kubejs/startup_scripts/item_registry.js
@@ -6,6 +6,10 @@ onEvent('item.registry', (event) => {
         'part_garden_cloche',
         'SAC',
         'central_windmill',
+        'advanced_extreme_circuit_board',
+        'Extreme_circuit_board',
+        'netherite_transistor',
+        'uranium_capacitor',
     ];
 
     generalItems.forEach((item) => {
