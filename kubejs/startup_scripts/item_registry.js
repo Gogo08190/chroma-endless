67abfa59145c115aacbb34ebb002da326659e2ee
mathieucrombe <mathieucrombe@gmail.com>
2022-06-12 15:54:44 +0200

    add unassembled fluid and storage

--- kubejs/startup_scripts/item_registry.js
+++ kubejs/startup_scripts/item_registry.js
@@ -9,7 +9,23 @@ onEvent('item.registry', (event) => {
         'advanced_extreme_circuit_board',
         'extreme_circuit_board',
         'netherite_transistor',
-        'uranium_capacitor'
+        'uranium_capacitor',
+        'unassembled_1k_storage_part',
+        'unassembled_4k_storage_part',
+        'unassembled_16k_storage_part',
+        'unassembled_64k_fluid_storage_part',
+        'unassembled_64k_storage_part',
+        'unassembled_256k_fluid_storage_part',
+        'unassembled_256k',
+        'unassembled_1024k_fluid_storage_part',
+        'unassembled_1024k',
+        'unassembled_4096k_fluid_storage_part',
+        'unassembled_4096k',
+        'unassembled_16384k_fluid',
+        'unassembled_16384k',
+        'unassembled_65536k_fluid',
+        'unassembled_262144k_fluid',
+        'unassembled_1048576k_fluid'
     ];
 
     generalItems.forEach((item) => {
