4b567c30f9c0c3179f1a256f04ce074f95d4f446
Gogo08 <gogo08190@gmail.com>
2022-06-18 09:50:37 +0200

    Add recipes for Enderio Alloy Smelter + test slice n splice

--- kubejs/server_scripts/chroma/kubejs/pack/recypetypes/custommachinery/alloysmelter.js
+++ kubejs/server_scripts/chroma/kubejs/pack/recypetypes/custommachinery/alloysmelter.js
@@ -1,12 +1,9 @@
-onEvent('recipes', (event) => { 
+onEvent('recipes', event => {
 
-
-    const recipes = [
-
-
-
-
-
-
-    ]
-});
+  event.recipes.custommachinery.custom_machine("enderio:alloy_smelter", 100)
+  .requireItem('pneumaticcraft:ingot_iron_compressed')
+  .requireItem('emendatusenigmatica:coal_dust')
+  .requireItem('prefab:block_compressed_obsidian')
+  .requireEnergyPerTick(20)
+  .produceItem(Item.of('enderioalloys:item_alloy_ingot_dark_steel', 1))
+})
