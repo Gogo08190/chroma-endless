b31448e26fdeedab27ac2004d4e0412bfbe35e24
Thebloodshadow <45766546+Thebloodshadow@users.noreply.github.com>
2022-06-18 15:07:52 +0200

    add : Some industiral forgoing recipes

--- kubejs/server_scripts/chroma/kubejs/pack/recypetypes/industrialforegoing/shaped.js
+++ kubejs/server_scripts/chroma/kubejs/pack/recypetypes/industrialforegoing/shaped.js
@@ -62,6 +62,85 @@ onEvent('recipes', (event) => {
           },
           id: 'industrialforegoing:machine_frame_pity'
       },
+      {
+          output: 'industrialforegoing:fluid_collector',
+          pattern: ['PSP', 'AMH', 'CRC'],
+          key: {
+              P:'#forge:plastic',
+              S:'pneumaticcraft:small_tank',
+              A:'refinedpipes:advanced_fluid_pipe',
+              M:'industrialforegoing:machine_frame_pity',
+              H:'create:hose_pulley',
+              C:'pneumaticcraft:compressed_iron_gear',
+              R:'thermal:rf_coil'
+          },
+          id: 'industrialforegoing:fluid_collector'
+      },
+      {
+          output: 'industrialforegoing:fluid_collector',
+          pattern: ['PSP', 'AMH', 'CRC'],
+          key: {
+              P:'#forge:plastic',
+              S:'pneumaticcraft:small_tank',
+              A:'refinedpipes:advanced_fluid_pipe',
+              M:'industrialforegoing:machine_frame_pity',
+              H:'create:spout',
+              C:'pneumaticcraft:compressed_iron_gear',
+              R:'thermal:rf_coil'
+          },
+          id: 'industrialforegoing:fluid_collector'
+      },
+      {
+          output: 'industrialforegoing:mob_slaughter_factory',
+          pattern: ['PTP', 'SMA', 'VRV'],
+          key: {
+              P:'#forge:plastic',
+              T:'pneumaticcraft:medium_tank',
+              S:Item.of('botania:terra_sword', '{Damage:0}').weakNBT(),
+              M:'industrialforegoing:machine_frame_pity',
+              A:Item.of('botania:terra_axe', '{Damage:0}').weakNBT(),
+              V:'enderioalloys:item_material_gear_vibrant',
+              R:'thermal:rf_coil'
+          },
+          id: 'industrialforegoing:mob_slaughter_factory'
+      },
+      {
+          output: 'industrialforegoing:dye_mixer',
+          pattern: ['PHP', 'HMH', 'GRG'],
+          key: {
+              P:'#forge:plastic',
+              H:'resourcefulbees:rgbee_honeycomb',
+              M:'industrialforegoing:machine_frame_pity',
+              G:'enderioalloys:item_material_gear_darksteel',
+              R:'thermal:rf_coil'
+          },
+          id: 'industrialforegoing:dye_mixer'
+      },
+      {
+          output: 'industrialforegoing:pitiful_generator',
+          pattern: ['SIS', 'BMB', 'SRS'],
+          key: {
+              S:'pneumaticcraft:reinforced_stone',
+              I:'#forge:ingots/brass',
+              B:'create:blaze_burner',
+              M:'industrialforegoing:machine_frame_pity',
+              R:'thermal:rf_coil'
+          },
+          id: 'industrialforegoing:pitiful_generator'
+      },
+      {
+          output: 'industrialforegoing:sewer',
+          pattern: ['PGP', 'BMB', 'BRB'],
+          key: {
+              P:'#forge:plastic',
+              G:'tardis:steam_grate',
+              B:'environmental:mud_bricks',
+              M:'industrialforegoing:machine_frame_pity',
+              R:'thermal:rf_coil'
+          },
+          id: 'industrialforegoing:sewer'
+      }
+
     ];
 
     recipes.forEach((recipe) => {
