29418979064eb4189822b19a5014456b7e6f6ea1
YukiiSamaa <thomas.levasseur.tl@gmail.com>
2022-06-22 10:22:58 +0200

    Change craft for enriched redstonen,carbon,diamond + add craft crying obsi dust

--- kubejs/server_scripts/chroma/kubejs/pack/recypetypes/mekanism/enriching.js
+++ kubejs/server_scripts/chroma/kubejs/pack/recypetypes/mekanism/enriching.js
@@ -9,6 +9,34 @@ onEvent('recipes', (event) => {
 
     const recipes = [
 
+      //Crying obsidian dust
+      {
+        input: 'minecraft:crying_obsidian',
+        output: 'kubejs:crying_obsidian_dust',
+        id: 'minecraft:crying_obsidian_dust'
+      },
+
+      //Carbon
+      {
+        input:'emendatusenigmatica:coke_gem',
+        output: 'mekanism:enriched_carbon',
+        id: 'mekanism:enriching/enriched/carbon'
+      },
+
+      //Redstone
+      {
+        input: 'extendedcrafting:redstone_ingot',
+        output: 'mekanism:enriched_redstone',
+        id: 'mekanism:enriching/enriched/redstone'
+      },
+
+      //Diamond
+      {
+        input: 'botania:mana_diamond',
+        output: 'mekanism:enriched_diamond',
+        id: 'mekanism:enriching/enriched/diamond'
+      }
+      
     ];
     recipes.forEach((recipe) => {
         event.recipes.mekanism.enriching(recipe.output, recipe.input).id(recipe.id);
