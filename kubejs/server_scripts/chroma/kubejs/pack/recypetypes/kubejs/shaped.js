f0ce894d09dc8d58557eea98775b2b01b60dd54c
Thebloodshadow <45766546+Thebloodshadow@users.noreply.github.com>
2022-06-23 19:45:27 +0200

    add : Unaseembled fluid parts

--- kubejs/server_scripts/chroma/kubejs/pack/recypetypes/kubejs/shaped.js
+++ kubejs/server_scripts/chroma/kubejs/pack/recypetypes/kubejs/shaped.js
@@ -21,7 +21,92 @@ onEvent('recipes', (event) => {
               C: 'thermal:rf_coil'
           },
           id: `${id_prefix}xnet_machine_frame`
-      }
+      },
+      /// Unassembled 256k fluid storage part
+      {
+          output: 'kubejs:unassembled_256k_fluid_storage_part',
+          pattern: ['QSQ', 'GCG', 'QGQ'],
+          key: {
+              Q: 'refinedstorage:quartz_enriched_iron',
+              S: '#forge:gems/silicon',
+              C: 'mekanism:alloy_infused',
+              G: 'thermal:obsidian_glass'
+          },
+          id: `${id_prefix}unassembled_256k_fluid_storage_part`
+      },
+      /// Unassembled 1024k fluid storage part
+      {
+          output: 'kubejs:unassembled_1024k_fluid_storage_part',
+          pattern: ['QSQ', 'GCG', 'QGQ'],
+          key: {
+              Q: 'refinedstorage:basic_processor',
+              S: '#forge:gems/silicon',
+              C: 'mekanism:alloy_infused',
+              G: 'kubejs:unassembled_256k_fluid_storage_part'
+          },
+          id: `${id_prefix}unassembled_1024k_fluid_storage_part`
+      },
+      ///Unassembled 4096k fluid storage part
+      {
+          output: 'kubejs:unassembled_4096k_fluid_storage_part',
+          pattern: ['QSQ', 'GCG', 'QGQ'],
+          key: {
+              Q: 'refinedstorage:improved_processor',
+              S: '#forge:gems/silicon',
+              C: 'mekanism:alloy_infused',
+              G: 'kubejs:unassembled_1024k_fluid_storage_part'
+          },
+          id: `${id_prefix}unassembled_4096k_fluid__storage_part`
+      },
+      ///Unassembled 16384k fluid storage part
+      {
+          output: 'kubejs:unassembled_16384k_fluid_storage_part',
+          pattern: ['QSQ', 'GCG', 'QGQ'],
+          key: {
+              Q: 'refinedstorage:improved_processor',
+              S: '#forge:gems/silicon',
+              C: 'mekanism:alloy_reinforced',
+              G: 'kubejs:unassembled_4096k_fluid_storage_part'
+          },
+          id: `${id_prefix}unassembled_16384k_fluid_storage_part`
+      },
+      ///Unassembled 65536k fluid storage part
+      {
+          output: 'kubejs:unassembled_65536k_fluid_storage_part',
+          pattern: ['QSQ', 'GCG', 'QGQ'],
+          key: {
+              Q: 'refinedstorage:improved_processor',
+              S: '#forge:gems/silicon',
+              C: 'mekanism:alloy_reinforced',
+              G: 'kubejs:unassembled_16384k_fluid_storage_part'
+          },
+          id: `${id_prefix}unassembled_65536k_fluid_storage_part`
+      },
+      ///Unassembled 262144k fluid storage part
+      {
+          output: 'kubejs:unassembled_262144k_fluid_storage_part',
+          pattern: ['QSQ', 'GCG', 'QGQ'],
+          key: {
+              Q: 'refinedstorage:advanced_processor',
+              S: '#forge:gems/silicon',
+              C: 'mekanism:alloy_atomic',
+              G: 'kubejs:unassembled_65536k_fluid_storage_part'
+          },
+          id: `${id_prefix}unassembled_262144k_fluid_storage_part`
+      },
+      ///Unassembled 1048576k fluid storage part
+      {
+          output: 'kubejs:unassembled_1048576k_fluid_storage_part',
+          pattern: ['QSQ', 'GCG', 'QGQ'],
+          key: {
+              Q: 'extrastorage:neural_processor',
+              S: '#forge:gems/silicon',
+              C: 'mekanism:alloy_atomic',
+              G: 'kubejs:unassembled_262144k_fluid_storage_part'
+          },
+          id: `${id_prefix}unassembled_1048576k_fluid_storage_part`
+      },
+
     ];
 
     recipes.forEach((recipe) => {
