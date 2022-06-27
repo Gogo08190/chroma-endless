2c909743c5676c12ef2a24e4103ce9014386f4ce
Gogo08 <gogo08190@gmail.com>
2022-06-27 22:35:01 +0200

    Change extended colors

--- kubejs/server_scripts/chroma/kubejs/pack/recypetypes/extendedcrafting/shaped.js
+++ kubejs/server_scripts/chroma/kubejs/pack/recypetypes/extendedcrafting/shaped.js
@@ -11,7 +11,42 @@ onEvent('recipes', (event) => {
           },
           id: 'extendedcrafting:frame'
       },
-
+      {
+          output: 'extendedcrafting:basic_table',
+          pattern: ['BCB', 'MIM', 'BPB'],
+          key: {
+              B: 'extendedcrafting:basic_component',
+              C: 'extendedcrafting:basic_catalyst',
+              M: 'create:mechanical_crafter',
+              I: 'minecraft:iron_block',
+              P: 'extendedcrafting:black_iron_slate'
+          },
+          id: 'extendedcrafting:basic_table'
+      },
+      {
+          output: 'extendedcrafting:compressor',
+          pattern: ['PBP', 'CEC', 'PFP'],
+          key: {
+              B: 'extendedcrafting:ultimate_component',
+              C: 'extendedcrafting:ultimate_catalyst',
+              P: 'extendedcrafting:black_iron_slate',
+              E: 'pneumaticcraft:electrostatic_compressor',
+              F: 'extendedcrafting:frame'
+          },
+          id: 'extendedcrafting:compressor'
+      },
+      {
+          output: 'extendedcrafting:crafting_core',
+          pattern: ['PBP', 'CEC', 'PFP'],
+          key: {
+              B: 'extendedcrafting:ultimate_component',
+              C: 'extendedcrafting:ultimate_catalyst',
+              P: 'extendedcrafting:black_iron_slate',
+              E: 'extrastorage:netherite_crafter',
+              F: 'extendedcrafting:frame'
+          },
+          id: 'extendedcrafting:crafting_core'
+      }
     ];
 
     recipes.forEach((recipe) => {
