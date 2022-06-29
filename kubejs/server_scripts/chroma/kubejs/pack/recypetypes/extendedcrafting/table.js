14d153c0aac8557532ce848691abcaea9197eb4e
koepss <pierrelode2@outlook.fr>
2022-06-29 21:43:26 +0200

    add craft Ctiers centifuge T3

--- kubejs/server_scripts/chroma/kubejs/pack/recypetypes/extendedcrafting/table.js
+++ kubejs/server_scripts/chroma/kubejs/pack/recypetypes/extendedcrafting/table.js
@@ -1,24 +1,61 @@
 onEvent("recipes", (event) => {
+  const id_prefix = 'chroma:pack/extendedcrafting/table/';
   event.custom(
     {
       "type": "extendedcrafting:shaped_table",
       "pattern": [
-        " A     ",
-        "  A  AA",
-        "AAAAA  ",
-        "A      ",
-        "AA     ",
-        "  AA AA",
-        "    AA "
+        "AAAAA",
+        "ABCBA",
+        "ACDCA",
+        "ABCBA",
+        "AAAAA"
       ],
       "key": {
         "A": {
-          "item": "kubejs:inert_luminessence"
+          "item": "extendedcrafting:luminessence"
+        },
+        "B": {
+          "item": "mekanism:ultimate_control_circuit"
+        },
+        "C": {
+          "item": "mekanism:alloy_atomic"
+        },
+        "D": {
+          "item": "resourcefulbees:elite_centrifuge_controller"
         }
       },
       "result": {
-        "item": ""
+        "item": "ctiers:centrifuge_controller_tier_3"
       }
-    }
-  ).id('recipeID')
+    }.id(`${id_prefix}ctiers:centrifuge_controller_tier_3`))
+
+    event.custom(
+        {
+      "type": "extendedcrafting:shaped_table",
+      "pattern": [
+        "AAAAA",
+        "ABBBA",
+        "ACDCA",
+        "ADCDA",
+        "AAAAA"
+      ],
+      "key": {
+        "A": {
+          "item": "extendedcrafting:luminessence"
+        },
+        "B": {
+          "item": "mekanism:alloy_atomic"
+        },
+        "C": {
+          "item": "mekanism:ultimate_control_circuit"
+        },
+        "D": {
+          "item": "resourcefulbees:elite_centrifuge_casing"
+        }
+      },
+      "result": {
+        "item": "3x ctiers:centrifuge_casing_tier_3"
+      }
+    }.id(`${id_prefix}ctiers:centrifuge_casing_tier_3`))
+
 });
