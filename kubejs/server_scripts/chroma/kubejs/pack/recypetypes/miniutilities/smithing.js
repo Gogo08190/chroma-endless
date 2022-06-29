966a1a98c3f20761a403d1b3bcc46bc5aac69f7e
Hugo <gogo08190@gmail.com>
2022-06-29 11:22:13 +0200

    Fix smithing function

--- kubejs/server_scripts/chroma/kubejs/pack/recypetypes/miniutilities/smithing.js
+++ kubejs/server_scripts/chroma/kubejs/pack/recypetypes/miniutilities/smithing.js
@@ -1,13 +1,14 @@
 onEvent('recipes', (event) => {
   const recipes = [
     {
+      input1: 'miniutilities:diamond_spikes',
+      input2: 'minecraft:netherite_block',
       output: 'miniutilities:netherite_spikes',
-      inputs: ['miniutilities:diamond_spikes', 'minecraft:netherite_block'],
       id: 'miniutilities:netherite_spikes_smithing'
     }
   ];
 
   recipes.forEach((recipe) => {
-    event.smithing(recipe.output, recipe.inputs).id(recipe.id);
+    event.smithing(recipe.output, recipe.input1, recipe.input2).id(recipe.id);
   });
 });
