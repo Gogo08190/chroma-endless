0af7aba932baf959e87f7da2b2b0adca270cfd3d
YukiiSamaa <thomas.levasseur.tl@gmail.com>
2022-06-30 21:20:22 +0200

    Fix errors

--- kubejs/server_scripts/chroma/kubejs/pack/recypetypes/mekanism/remove.js
+++ kubejs/server_scripts/chroma/kubejs/pack/recypetypes/mekanism/remove.js
@@ -1,17 +1,35 @@
-{
-  //Combiner
-  'mekanism:combiner',
-  'mekanism:basic_combining_factory',
-  'mekanism:advanced_combining_factory',
-  'mekanism:elite_combining_factory',
-  'mekanism:ultimate_combining_factory',
+// priority: 100
 
-  //Control Circuit
-  'mekanism:control_circuit/advanced',
-  'mekanism:control_circuit/elite',
-  'mekanism:control_circuit/ultimate',
+onEvent('recipes', (event) => {
+    const idRemovals = [
 
-  //infusion type coal
-  'mekanism:infusion_conversion/carbon/from_charcoal_block',
-  'mekanism:infusion_conversion/carbon/from_charcoal',
-}
+      //Combiner
+      'mekanism:combiner',
+      'mekanism:basic_combining_factory',
+      'mekanism:advanced_combining_factory',
+      'mekanism:elite_combining_factory',
+      'mekanism:ultimate_combining_factory',
+
+      //Control Circuit
+      'mekanism:control_circuit/advanced',
+      'mekanism:control_circuit/elite',
+      'mekanism:control_circuit/ultimate',
+
+      //infusion type coal
+      'mekanism:infusion_conversion/carbon/from_charcoal_block',
+      'mekanism:infusion_conversion/carbon/from_charcoal',
+
+    ];
+
+    const outputRemovals = [
+      'occultism:dictionary_of_spirits'
+    ];
+
+    idRemovals.forEach((id) => {
+        event.remove({ id: id });
+    });
+
+    outputRemovals.forEach((output) => {
+        event.remove({ output: output });
+    });
+});
