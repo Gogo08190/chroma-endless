d8e7797a4e7ea97d463de45668b275a21d419e33
YukiiSamaa <thomas.levasseur.tl@gmail.com>
2022-06-30 12:20:07 +0200

    Fix texture terrasteel infuse

--- kubejs/startup_scripts/infuse_type_registry.js
+++ kubejs/startup_scripts/infuse_type_registry.js
@@ -1,5 +1,5 @@
 onEvent('infuse_type.registry', event => {
 
-  event.create('terrasteel_infuse_type').color(4846939).texture('kubejs:infuse_types/terrasteel_infuse_type.png')
+  event.create('terrasteel_infuse_type').color(4706589).texture('kubejs:infuse_types/terrasteel_infuse_type_texture')
 
 })
