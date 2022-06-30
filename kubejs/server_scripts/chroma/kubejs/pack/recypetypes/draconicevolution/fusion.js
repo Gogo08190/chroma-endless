691c1a20dd2a5d015e28b0feed70fd9298b26183
Hugo <gogo08190@gmail.com>
2022-06-30 14:21:05 +0200

    Add another example

--- kubejs/server_scripts/chroma/kubejs/pack/recypetypes/draconicevolution/fusion.js
+++ kubejs/server_scripts/chroma/kubejs/pack/recypetypes/draconicevolution/fusion.js
@@ -36,4 +36,41 @@ onEvent("recipes", (event) => {
             },
         ],
     }).id('recipeID');
+    
+    event.recipes.draconicevolution.fusion_crafting({
+        result: {
+            item: "item",
+        },
+        catalyst: {
+            item: "item",
+        },
+        total_energy: 10000,
+        tier: "WYVERN",
+        ingredients: [
+            {
+                item: "item",
+            },
+            {
+                item: "item",
+            },
+            {
+                item: "item",
+            },
+            {
+                item: "item",
+            },
+            {
+                item: "item",
+            },
+            {
+                item: "item",
+            },
+            {
+                item: "item",
+            },
+            {
+                item: "item",
+            },
+        ],
+    }).id('recipeID');
 });
