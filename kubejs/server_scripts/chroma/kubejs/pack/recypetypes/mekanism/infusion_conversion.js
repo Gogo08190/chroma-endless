e315a3a7b0a1ad07443cdfdf908b3b9d15920edc
YukiiSamaa <thomas.levasseur.tl@gmail.com>
2022-06-30 20:46:17 +0200

    Add infuse for block and enriched terrasteel, add name for infuse terraasteel

--- kubejs/server_scripts/chroma/kubejs/pack/recypetypes/mekanism/infusion_conversion.js
+++ kubejs/server_scripts/chroma/kubejs/pack/recypetypes/mekanism/infusion_conversion.js
@@ -5,6 +5,18 @@ onEvent('recipes', (event) => {
         input: { ingredient: { item: 'botania:terrasteel_ingot' } },
         output: { amount: 10, infuse_type: 'kubejs:terrasteel_infuse_type' }
       },
+
+      //Block Terrasteel
+      {
+        input: { ingredient: { item: 'botania:terrasteel_block' } },
+        output: { amount: 90, infuse_type: 'kubejs:terrasteel_infuse_type' }
+      },
+
+      //Enriched Terrasteel
+      {
+        input: { ingredient: { item: 'kubejs:enriched_terrasteel' } },
+        output: { amount: 80, infuse_type: 'kubejs:terrasteel_infuse_type' }
+      },
     ]
 
     recipes.forEach((recipe) => {
