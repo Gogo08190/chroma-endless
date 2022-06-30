12ee1174e3a4c33075f7752894625ed989dc1cfc
Gogo08 <gogo08190@gmail.com>
2022-06-30 13:31:40 +0200

    Travel Anchor recipes

---
+++ kubejs/server_scripts/chroma/kubejs/pack/recypetypes/travel_anchor/shaped.js
@@ -0,0 +1,27 @@
+onEvent('recipes', (event) => {
+    const recipes = [
+        {
+            output: 'travel_anchors:travel_staff',
+            pattern: ['  P', ' E ', 'E'],
+            key: {
+                E: 'enderioalloys:item_alloy_ingot_electrical_steel',
+                P: 'enderioalloys:item_material_vibrant_crystal'
+            },
+            id: 'travel_anchors:travel_staff'
+        },
+        {
+            output: 'travel_anchors:travel_anchor',
+            pattern: ['EDE', 'DPD', 'EDE'],
+            key: {
+                E: 'enderioalloys:item_alloy_ingot_electrical_steel',
+                P: 'enderioalloys:item_material_vibrant_crystal',
+                D: 'enderioalloys:item_alloy_ingot_dark_steel'
+            },
+            id: 'travel_anchors:travel_anchor'
+        }
+    ];
+
+    recipes.forEach((recipe) => {
+        event.shaped(recipe.output, recipe.pattern, recipe.key).id(recipe.id);
+    });
+});
