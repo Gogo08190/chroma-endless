06cd9d20d893692b99e454e3e2df5a21e562e797
Hugo <gogo08190@gmail.com>
2022-06-30 14:19:41 +0200

    Fusion recipe draconic

---
+++ kubejs/server_scripts/chroma/kubejs/pack/recypetypes/draconicevolution/fusion.js
@@ -0,0 +1,39 @@
+onEvent("recipes", (event) => {
+
+    event.recipes.draconicevolution.fusion_crafting({
+        result: {
+            item: "item",
+        },
+        catalyst: {
+            item: "item",
+        },
+        total_energy: 10000,
+        tier: "WYVERN",
+        ingredients: [
+            {
+                item: "item",
+            },
+            {
+                item: "item",
+            },
+            {
+                item: "item",
+            },
+            {
+                item: "item",
+            },
+            {
+                item: "item",
+            },
+            {
+                item: "item",
+            },
+            {
+                item: "item",
+            },
+            {
+                item: "item",
+            },
+        ],
+    }).id('recipeID');
+});
