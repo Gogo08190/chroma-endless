197e8232c8f050a84467e6ab98dd195fdb9467ae
Gogo08 <gogo08190@gmail.com>
2022-07-06 13:16:37 +0200

    Remove frame recipes

--- kubejs/server_scripts/chroma/kubejs/pack/recypetypes/envirocore/remove.js
+++ kubejs/server_scripts/chroma/kubejs/pack/recypetypes/envirocore/remove.js
@@ -12,6 +12,10 @@ onEvent('recipes', (event) => {
       'envirocore:erodium_frame',
       'envirocore:kyronite_frame',
       'envirocore:pladium_frame',
+      'envirocore:ionite_frame',
+      'envirocore:aethium_frame',
+      'envirocore:nanorite_frame',
+      'envirocore:xerothium_frame'
     ];
 
     idRemovals.forEach((id) => {
