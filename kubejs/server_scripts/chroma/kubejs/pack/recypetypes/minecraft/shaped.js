8f033f9b73b07ae2a10b9103f85854e5e16b3dec
Gogo08190 <gogo08190@gmail.com>
2022-07-11 20:44:01 +0200

    Remove Emendatus hammer

--- kubejs/server_scripts/chroma/kubejs/pack/recypetypes/minecraft/shaped.js
+++ kubejs/server_scripts/chroma/kubejs/pack/recypetypes/minecraft/shaped.js
@@ -27,6 +27,76 @@ onEvent('recipes', (event) => {
             O: 'minecraft:obsidian'
           },
           id: 'minecraft:crying_obsidian'
+        },
+        //Furnace
+        {
+          output: 'minecraft:furnace',
+          pattern:['CCC','CFC','CBC'],
+          key: {
+            C: '#quark:stone_tool_materials',
+            B: '#forge:storage_blocks/coal',
+            F: 'minecraft:campfire'
+          },
+          id: 'minecraft:furnace'
+        },
+        //Piston
+        {
+          output: 'minecraft:piston',
+          pattern:['WPW','CSC','CRC'],
+          key: {
+            C: 'minecraft:smooth_stone',
+            W: '#minecraft:planks',
+            P: '#forge:plates/iron_aluminum',
+            S: 'create:shaft',
+            R: '#forge:dusts/redstone'
+          },
+          id: 'minecraft:piston'
+        },
+        //Blast Furnace
+        {
+          output: 'minecraft:blast_furnace',
+          pattern:['PPP','PFP','CLC'],
+          key: {
+            C: 'minecraft:smooth_stone',
+            P: '#forge:plates/iron_aluminum',
+            F: 'minecraft:furnace',
+            L: 'minecraft:campfire'
+          },
+          id: 'minecraft:blast_furnace'
+        },
+        //Smoker
+        {
+          output: 'minecraft:smoker',
+          pattern:['SWS','WFW','SLS'],
+          key: {
+            F: 'minecraft:furnace',
+            L: 'minecraft:campfire',
+            S: '#forge:rods/wooden',
+            W: '#minecraft:logs'
+          },
+          id: 'minecraft:smoker'
+        },
+        //Enchanting Table
+        {
+          output: 'minecraft:enchanting_table',
+          pattern:[' B ','DOD','OOO'],
+          key: {
+            B: 'minecraft:book',
+            D: '#forge:plates/diamond',
+            O: 'minecraft:obsidian'
+          },
+          id: 'minecraft:enchanting_table'
+        },
+        //Brewing Stand
+        {
+          output: 'minecraft:brewing_stand',
+          pattern:['NBN',' B ','AAA'],
+          key: {
+            N: '#forge:nuggets/iron_invar',
+            B: '#forge:rods/blaze',
+            A: '#forge:ingots/andesite_alloy'
+          },
+          id: 'minecraft:brewing_stand'
         }
     ];
 
