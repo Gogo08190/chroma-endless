45721096c7e234ac7cbbea7500d5624de3801df1
Gogo08 <gogo08190@gmail.com>
2022-07-14 16:08:28 +0200

    Tiny Compact Machine recipe

--- kubejs/server_scripts/chroma/kubejs/pack/recypetypes/masterful_machinery/compact_factory.js
+++ kubejs/server_scripts/chroma/kubejs/pack/recypetypes/masterful_machinery/compact_factory.js
@@ -1,7 +1,18 @@
 onEvent('recipes', (event) => {
     const id_prefix = 'chroma:pack/masterful_machinery/compact_factory/';
     const recipes = [
-
+      {
+          outputs: [{ type: 'masterfulmachinery:items', data: { item: 'compactmachines:machine_tiny', count: 1 } }],
+          inputs: [
+              { type: 'masterfulmachinery:items', data: { item: 'compactmachines:wall', count: 8 } },
+              { type: 'masterfulmachinery:items', data: { item: 'botania:livingwood', count: 1 } },
+              { type: 'masterfulmachinery:items', data: { item: 'immersiveengineering:component_steel', count: 2 } },
+              { type: 'masterfulmachinery:items', data: { item: 'emendatusenigmatica:brass_gear', count: 1 } },
+              { type: 'masterfulmachinery:energy', perTick: true, data: { amount: 500 } }
+          ],
+          ticks: 240,
+          id: 'compactmachines:machine_tiny'
+      },
     ];
 
     recipes.forEach((recipe) => {
