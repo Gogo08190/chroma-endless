093f6244ff7ccda2e14a6f6bafd53a23f21d3a33
koepss <pierrelode2@outlook.fr>
2022-07-18 21:26:08 +0200

    add craft manasteel bee

--- kubejs/server_scripts/chroma/kubejs/pack/recypetypes/botania/bee_infusion.js
+++ kubejs/server_scripts/chroma/kubejs/pack/recypetypes/botania/bee_infusion.js
@@ -5,7 +5,7 @@ onEvent('recipes', (event) => {
         {
             input: Item.of('resourcefulbees:bee_jar', { Entity: 'resourcefulbees:iron_bee' }),
             output: Item.of('resourcefulbees:bee_jar', { Entity: 'resourcefulbees:manasteel_bee' }),
-            mana: 99999,
+            mana: 100000,
             id: `${id_prefix}manasteel_bee_jar`
         }
     ];
