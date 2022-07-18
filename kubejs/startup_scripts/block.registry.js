1486eb0cf2966805c7d6ef9bf44704ce9a2333f8
YukiiSamaa <thomas.levasseur.tl@gmail.com>
2022-07-18 17:40:19 +0200

    Add block gaia block

--- kubejs/startup_scripts/block.registry.js
+++ kubejs/startup_scripts/block.registry.js
@@ -2,7 +2,8 @@ onEvent('block.registry', (event) => {
     const blocks = [
         { name: 'spirit_attuned_block', material: 'iron', hardness: 5 },
         { name: 'xnet_machine_frame', material: 'iron', hardness: 5 },
-        { name: 'sac', material: 'iron', hardness: 5 }
+        { name: 'sac', material: 'iron', hardness: 5 },
+        { name: 'gaia_block', material: 'iron', hardness: 5}
     ];
 
     blocks.forEach((block) => {
