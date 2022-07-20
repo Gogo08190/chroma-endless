7a4b4bbf10cd71201f5ce919ac6f10dc405bb067
Nolifertu/Victor <lerouxvictor3@gmail.com>
2022-07-20 22:19:05 +0200

    edit betterend craft with alloying :wrench:

--- kubejs/server_scripts/chroma/kubejs/pack/recypetypes/betterend/alloying.js
+++ kubejs/server_scripts/chroma/kubejs/pack/recypetypes/betterend/alloying.js
@@ -4,13 +4,13 @@ onEvent('recipes', (event) => {
         recipes:
             [
                 {
-                    ingredients: [{ item: 'architects_palette:ender_pearl_block' }, { item: 'emendatusenigmatica:steel_ingot' }],
+                    ingredients: [{ item: 'emendatusenigmatica:enderium_ingot' }, { item: 'emendatusenigmatica:steel_ingot' }],
                     result: 'betterendforge:terminite_ingot',
                     experience: 20,
                     smelttime: 500
                 },
                 {
-                    ingredients: [{ item: 'minecraft:netherite_block' }, { item: 'betterendforge:terminite_ingot' }],
+                    ingredients: [{ item: 'createdeco:netherite_sheet' }, { item: 'betterendforge:terminite_ingot' }],
                     result: 'betterendforge:aeternium_ingot',
                     experience: 25,
                     smelttime: 1000
