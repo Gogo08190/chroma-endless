331edfa79ff526fc37e4b783f5704b1c35606279
Gogo08 <gogo08190@gmail.com>
2022-07-22 09:47:56 +0200

    Remove smithing table from betterend

--- kubejs/server_scripts/chroma/kubejs/pack/recypetypes/minecraft/remove.js
+++ kubejs/server_scripts/chroma/kubejs/pack/recypetypes/minecraft/remove.js
@@ -5,7 +5,9 @@ onEvent('recipes', (event) => {
       'minecraft:blaze_powder',
       'byg:therium_shard_and_steel',
       'alexsmobs:flint_n_steel_dropbear_claw',
-      'tconstruct:common/glass/vanilla/end_crystal'
+      'tconstruct:common/glass/vanilla/end_crystal',
+      'betterendforge:smithing_table_from_terminite_ingot',
+      'betterendforge:smithing_table_from_thallasium_ingot'
     ];
 
     const outputRemovals = [
