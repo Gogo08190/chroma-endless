3ff208704f622494e722c03a4c34298c1c105196
Gogo08 <gogo08190@gmail.com>
2022-07-26 14:08:41 +0200

    Betterend bee recipe

--- kubejs/server_scripts/chroma/kubejs/pack/recypetypes/betterend/infusion.js
+++ kubejs/server_scripts/chroma/kubejs/pack/recypetypes/betterend/infusion.js
@@ -31,6 +31,75 @@ onEvent('recipes', (event) => {
                 ],
                 time: 250,
                 id: 'extendedcrafting:ender_alternator'
+            },
+            {
+                input: Item.of('resourcefulbees:bee_jar', { Entity: 'resourcefulbees:ender_bee' }).weakNBT().toJson(),
+                output: Item.of('resourcefulbees:bee_jar', { Entity: 'resourcefulbees:thallasium_bee' }),
+                output: {
+                  id: 'resourcefulbees:bee_jar',
+                  Count: 1,
+                  tag: {
+                    Entity: 'resourcefulbees:thallasium_bee'
+                  }
+                },
+                catalysts: [
+                    { index: 0, item: 'betterendforge:aurora_crystal' },
+                    { index: 1, tag: 'forge:storage_blocks/thallasium' },
+                    { index: 2, item: 'betterendforge:aurora_crystal' },
+                    { index: 3, tag: 'forge:storage_blocks/thallasium' },
+                    { index: 4, item: 'betterendforge:aurora_crystal' },
+                    { index: 5, tag: 'forge:storage_blocks/thallasium' },
+                    { index: 6, item: 'betterendforge:aurora_crystal' },
+                    { index: 7, tag: 'forge:storage_blocks/thallasium' },
+                ],
+                time: 250,
+                id: `${id_prefix}thallasium_bee`
+            },
+            {
+                input: Item.of('resourcefulbees:bee_jar', { Entity: 'resourcefulbees:thallasium_bee' }).weakNBT().toJson(),
+                output: Item.of('resourcefulbees:bee_jar', { Entity: 'resourcefulbees:terminite_bee' }),
+                output: {
+                  id: 'resourcefulbees:bee_jar',
+                  Count: 1,
+                  tag: {
+                    Entity: 'resourcefulbees:terminite_bee'
+                  }
+                },
+                catalysts: [
+                    { index: 0, item: 'betterendforge:smaragdant_crystal' },
+                    { index: 1, tag: 'forge:storage_blocks/terminite' },
+                    { index: 2, tag: 'forge:storage_blocks/steel' },
+                    { index: 3, tag: 'forge:storage_blocks/terminite' },
+                    { index: 4, item: 'betterendforge:smaragdant_crystal'  },
+                    { index: 5, tag: 'forge:storage_blocks/terminite' },
+                    { index: 6, tag: 'forge:storage_blocks/steel' },
+                    { index: 7, tag: 'forge:storage_blocks/terminite' },
+                ],
+                time: 250,
+                id: `${id_prefix}terminite_bee`
+            },
+            {
+                input: Item.of('resourcefulbees:bee_jar', { Entity: 'resourcefulbees:terminite_bee' }).weakNBT().toJson(),
+                output: Item.of('resourcefulbees:bee_jar', { Entity: 'resourcefulbees:aeternium_bee' }),
+                output: {
+                  id: 'resourcefulbees:bee_jar',
+                  Count: 1,
+                  tag: {
+                    Entity: 'resourcefulbees:aeternium_bee'
+                  }
+                },
+                catalysts: [
+                    { index: 0, item: 'betterendforge:eternal_crystal' },
+                    { index: 1, tag: 'forge:storage_blocks/aeternium' },
+                    { index: 2, item: 'betterendforge:eternal_crystal' },
+                    { index: 3, tag: 'forge:storage_blocks/aeternium' },
+                    { index: 4, item: 'betterendforge:eternal_crystal'  },
+                    { index: 5, tag: 'forge:storage_blocks/aeternium' },
+                    { index: 6, item: 'betterendforge:eternal_crystal' },
+                    { index: 7, tag: 'forge:storage_blocks/aeternium' },
+                ],
+                time: 250,
+                id: `${id_prefix}aeternium_bee`
             }
         ]
     };
