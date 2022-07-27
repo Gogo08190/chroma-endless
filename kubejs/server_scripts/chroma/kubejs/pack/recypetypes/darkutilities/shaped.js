e6fc4f192b939e8d783304dfe4bedcaabcfb2716
benji2708 <benjamin.jacques27@gmail.com>
2022-07-27 22:06:57 +0200

    Add folder and file shaped.js for dark utilities

---
+++ kubejs/server_scripts/chroma/kubejs/pack/recypetypes/darkutilities/shaped.js
@@ -0,0 +1,19 @@
+
+/*onEvent('recipes', (event) => {
+    const recipes = [
+      {
+        output:                         ,
+        pattern: ['AAA', 'BBB', 'CCC'],
+        key: {
+            A:                          ,
+            B:                          ,
+            C:
+        },
+        id:
+      }
+];
+
+    recipes.forEach((recipe) => {
+        event.shaped(recipe.output, recipe.pattern, recipe.key).id(recipe.id);
+    });
+});*/
