8b0ffc6f4da9de7f8da8727c6f4886c4ef4e3f4f
Gogo08 <gogo08190@gmail.com>
2022-07-29 08:44:25 +0200

    Fix block name

--- kubejs/startup_scripts/block.registry.js
+++ kubejs/startup_scripts/block.registry.js
@@ -1,13 +1,13 @@
 onEvent('block.registry', (event) => {
     const blocks = [
-        { name: 'spirit_attuned_block', material: 'iron', hardness: 5 },
-        { name: 'xnet_machine_frame', material: 'iron', hardness: 5 },
-        { name: 'sac', material: 'iron', hardness: 5 },
-        { name: 'gaia_spirit_block', material: 'iron', hardness: 5},
-        { name: 'energized_uranium_block', material: 'iron', hardness: 5}
+        { name: 'spirit_attuned_block', material: 'iron', hardness: 5, display_name: "Block of Spirit Attuned Gem" },
+        { name: 'xnet_machine_frame', material: 'iron', hardness: 5, display_name: "Xnet Machine Frame" },
+        { name: 'sac', material: 'iron', hardness: 5, display_name: "SAC" },
+        { name: 'gaia_spirit_block', material: 'iron', hardness: 5, display_name: "Block of Gaia Spirit" },
+        { name: 'energized_uranium_block', material: 'iron', hardness: 5, display_name: "Block of Energized Uranium" }
     ];
 
     blocks.forEach((block) => {
-        event.create(block.name).material(block.material).hardness(block.hardness);
+        event.create(block.name).material(block.material).hardness(block.hardness).displayName(block.display_name);
     });
 });
