9773effd95fa1aec413e9e743c8f4f0ed04d4937
Gogo08 <gogo08190@gmail.com>
2022-08-01 17:24:40 +0200

    Fix infusion conversion id

--- kubejs/server_scripts/chroma/kubejs/pack/recypetypes/mekanism/infusion_conversion.js
+++ kubejs/server_scripts/chroma/kubejs/pack/recypetypes/mekanism/infusion_conversion.js
@@ -6,21 +6,21 @@ onEvent('recipes', (event) => {
       {
         input: { ingredient: { item: 'kubejs:energized_uranium' } },
         output: { amount: 10, infuse_type: 'kubejs:uranium' },
-        `${id_prefix}uranium`
+        id: `${id_prefix}uranium`
       },
 
       //Block Uranium
       {
         input: { ingredient: { item: 'emendatusenigmatica:uranium_block' } },
         output: { amount: 90, infuse_type: 'kubejs:uranium' },
-        `${id_prefix}uranium_from_block`
+        id: `${id_prefix}uranium_from_block`
       },
 
       //Enriched Uranium WIP
       {
         input: { ingredient: { item: 'kubejs:enriched_terrasteel' } },
         output: { amount: 80, infuse_type: 'kubejs:uranium' },
-        `${id_prefix}uranium_from_enriched`
+        id: `${id_prefix}uranium_from_enriched`
       },
 
       //Kyronite Crystal
