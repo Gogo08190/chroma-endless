82c8d66ddadf8c08cefd94b044c90c88f7389e66
Gogo08 <gogo08190@gmail.com>
2022-08-05 13:12:30 +0200

    Ender crafter recipes

--- kubejs/server_scripts/chroma/kubejs/pack/recypetypes/extendedcrafting/ender_crafter.js
+++ kubejs/server_scripts/chroma/kubejs/pack/recypetypes/extendedcrafting/ender_crafter.js
@@ -1,17 +1,42 @@
 onEvent('recipes', (event) => {
     const id_prefix = 'chroma:pack/extendedcrafting/ender_crafter/';
     const recipes = [
-       /* {
-          pattern: [' A ', 'BAB', ' A '],
+      {
+          pattern: ['ABA', 'CDC', 'AEA'],
           key: {
-            A: 'ItemA',
-            B: 'ItemB'
+            A: { item: 'envirocore:structure_panel' },
+            B: { item: 'mekanism:hdpe_sheet' },
+            C: { item: 'extendedcrafting:black_iron_ingot' },
+            D: { item: 'mekanism:upgrade_speed' },
+            E: { item: 'create:shaft' }
           },
-          result: {
-            {item: 'extendedcrafting:enhanced_ender_ingot', count: 1}
+          result: { item: 'masterfulmachinery:cuttingmachine_cuttingmachine_speed_port_create_rotation_input', count: 1 },
+          id: `${id_prefix}cuttingmachine_cuttingmachine_speed_port_create_rotation_input`
+      },
+      {
+          pattern: ['ABA', 'CDC', 'AEA'],
+          key: {
+            A: { item: 'envirocore:structure_panel' },
+            B: { item: 'mekanism:hdpe_sheet' },
+            C: { item: 'enderioalloys:item_alloy_ingot_redstone_alloy' },
+            D: { item: 'expandedstorage:netherite_chest' },
+            E: { item: 'refinedpipes:advanced_item_pipe' }
           },
-          id: `${id_prefix}blabla`
-      } */
+          result: { item: 'masterfulmachinery:cuttingmachine_cuttingmachine_item_port_items_output', count: 1 },
+          id: `${id_prefix}cuttingmachine_cuttingmachine_item_port_items_output`
+      },
+      {
+          pattern: ['ABA', 'CDC', 'AEA'],
+          key: {
+            A: { item: 'envirocore:structure_panel' },
+            B: { item: 'mekanism:hdpe_sheet' },
+            C: { item: 'enderioalloys:item_alloy_ingot_vibrant_alloy' },
+            D: { item: 'expandedstorage:netherite_chest' },
+            E: { item: 'refinedpipes:advanced_item_pipe' }
+          },
+          result: { item: 'masterfulmachinery:cuttingmachine_cuttingmachine_item_port_items_input', count: 1 },
+          id: `${id_prefix}cuttingmachine_cuttingmachine_item_port_items_input`
+      }
     ];
 
     recipes.forEach((recipe) => {
