e8a2fefed9d571427dc181c3075b728aaf3d38fb
koepss <pierrelode2@outlook.fr>
2022-08-16 16:45:27 +0200

    add craft bee induction smelter

--- kubejs/server_scripts/chroma/kubejs/pack/recypetypes/thermal/machine/induction_smelter.js
+++ kubejs/server_scripts/chroma/kubejs/pack/recypetypes/thermal/machine/induction_smelter.js
@@ -5,7 +5,23 @@ onEvent('recipes', (event) => {
           inputs: ['pneumaticcraft:compressed_brick_tile', 'pneumaticcraft:advanced_pcb', Item.of('thermal:energy_cell').ignoreNBT()],
           outputs: ['masterfulmachinery:assemblylaser_assembly_energy_port_energy_input'],
           id: `${id_prefix}assemblylaser_assembly_energy_port_energy_input`
+      },
+      {
+          inputs: [Item.of('resourcefulbees:bee_jar', { Entity: 'minecraft:bee' }), 'emendatusenigmatica:bitumen_block'],
+          outputs: [Item.of('resourcefulbees:bee_jar', { Entity: 'resourcefulbees:bitumen_bee' })],
+          id: `${id_prefix}bitumen_bee`
+      },
+      {
+          inputs: [Item.of('resourcefulbees:bee_jar', { Entity: 'minecraft:bee' }), 'emendatusenigmatica:potassium_nitrate_block'],
+          outputs: [Item.of('resourcefulbees:bee_jar', { Entity: 'resourcefulbees:potassium_bee' })],
+          id: `${id_prefix}potassium_bee`
+      },
+      {
+          inputs: [Item.of('resourcefulbees:bee_jar', { Entity: 'resourcefulbees:copper_bee' }), Item.of('resourcefulbees:bee_jar', { Entity: 'resourcefulbees:cobalt_bee' }), Item.of('resourcefulbees:bee_jar', { Entity: 'resourcefulbees:nether_quartz_bee' })],
+          outputs: [Item.of('resourcefulbees:bee_jar', { Entity: 'resourcefulbees:hepatizon_bee' })],
+          id: `${id_prefix}potassium_bee`
       }
+
     ];
 
     recipes.forEach((recipe) => {
