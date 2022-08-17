bbb41a3e41fc1497051bd2c79bd28cc26a47a5bf
mathieucrombe <mathieucrombe@gmail.com>
2022-08-17 12:54:59 +0200

    add lithium dust block

--- kubejs/startup_scripts/block.registry.js
+++ kubejs/startup_scripts/block.registry.js
@@ -3,7 +3,8 @@ onEvent('block.registry', (event) => {
         { name: 'spirit_attuned_block', material: 'iron', hardness: 5, display_name: "Block of Spirit Attuned Gem" },
         { name: 'xnet_machine_frame', material: 'iron', hardness: 5, display_name: "Xnet Machine Frame" },
         { name: 'gaia_spirit_block', material: 'iron', hardness: 5, display_name: "Block of Gaia Spirit" },
-        { name: 'energized_uranium_block', material: 'iron', hardness: 5, display_name: "Block of Energized Uranium" }
+        { name: 'energized_uranium_block', material: 'iron', hardness: 5, display_name: "Block of Energized Uranium" },
+        { name: 'lithium_dust_block', material: 'sand', hardness: 5, display_name: "Lithium Dust Block" }
     ];
 
     blocks.forEach((block) => {
