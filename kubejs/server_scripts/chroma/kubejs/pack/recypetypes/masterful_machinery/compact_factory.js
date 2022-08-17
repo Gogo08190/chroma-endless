951b239fe51b84058a1c0c4bfe71ffc0e26fe049
Gogo08 <gogo08190@gmail.com>
2022-08-17 14:13:27 +0200

    Add craft for compact wall in master compact

--- kubejs/server_scripts/chroma/kubejs/pack/recypetypes/masterful_machinery/compact_factory.js
+++ kubejs/server_scripts/chroma/kubejs/pack/recypetypes/masterful_machinery/compact_factory.js
@@ -4,10 +4,10 @@ onEvent('recipes', (event) => {
       {
           outputs: [{ type: 'masterfulmachinery:items', data: { item: 'compactmachines:machine_tiny', count: 1 } }],
           inputs: [
-              { type: 'masterfulmachinery:items', data: { item: 'compactmachines:wall', count: 8 } },
-              { type: 'masterfulmachinery:items', data: { item: 'botania:livingwood', count: 1 } },
-              { type: 'masterfulmachinery:items', data: { item: 'immersiveengineering:component_steel', count: 2 } },
-              { type: 'masterfulmachinery:items', data: { item: 'emendatusenigmatica:brass_gear', count: 1 } },
+              { type: 'masterfulmachinery:items', data: { item: 'compactmachines:wall', count: 8 }, consumeInstantly: true },
+              { type: 'masterfulmachinery:items', data: { item: 'botania:livingwood', count: 1 }, consumeInstantly: true },
+              { type: 'masterfulmachinery:items', data: { item: 'immersiveengineering:component_steel', count: 2 }, consumeInstantly: true },
+              { type: 'masterfulmachinery:items', data: { item: 'emendatusenigmatica:brass_gear', count: 1 }, consumeInstantly: true },
               { type: 'masterfulmachinery:energy', perTick: true, data: { amount: 500 } }
           ],
           ticks: 150,
@@ -16,10 +16,10 @@ onEvent('recipes', (event) => {
       {
           outputs: [{ type: 'masterfulmachinery:items', data: { item: 'compactmachines:machine_small', count: 1 } }],
           inputs: [
-              { type: 'masterfulmachinery:items', data: { item: 'compactmachines:wall', count: 16 } },
-              { type: 'masterfulmachinery:items', data: { item: 'elementalcraft:drenched_iron_block', count: 1 } },
-              { type: 'masterfulmachinery:items', data: { item: 'extendedcrafting:redstone_ingot', count: 1 } },
-              { type: 'masterfulmachinery:items', data: { item: 'compactmachines:machine_tiny', count: 1 } },
+              { type: 'masterfulmachinery:items', data: { item: 'compactmachines:wall', count: 16 }, consumeInstantly: true },
+              { type: 'masterfulmachinery:items', data: { item: 'elementalcraft:drenched_iron_block', count: 1 }, consumeInstantly: true },
+              { type: 'masterfulmachinery:items', data: { item: 'extendedcrafting:redstone_ingot', count: 1 }, consumeInstantly: true },
+              { type: 'masterfulmachinery:items', data: { item: 'compactmachines:machine_tiny', count: 1 }, consumeInstantly: true },
               { type: 'masterfulmachinery:energy', perTick: true, data: { amount: 750 } }
           ],
           ticks: 150,
@@ -28,10 +28,10 @@ onEvent('recipes', (event) => {
       {
           outputs: [{ type: 'masterfulmachinery:items', data: { item: 'compactmachines:machine_normal', count: 1 } }],
           inputs: [
-              { type: 'masterfulmachinery:items', data: { item: 'compactmachines:wall', count: 32 } },
-              { type: 'masterfulmachinery:items', data: { item: 'emendatusenigmatica:gold_plate', count: 8 } },
-              { type: 'masterfulmachinery:items', data: { item: 'sophisticatedbackpacks:advanced_compacting_upgrade', count: 1 } },
-              { type: 'masterfulmachinery:items', data: { item: 'compactmachines:machine_small', count: 1 } },
+              { type: 'masterfulmachinery:items', data: { item: 'compactmachines:wall', count: 32 }, consumeInstantly: true },
+              { type: 'masterfulmachinery:items', data: { item: 'emendatusenigmatica:gold_plate', count: 8 }, consumeInstantly: true },
+              { type: 'masterfulmachinery:items', data: { item: 'sophisticatedbackpacks:advanced_compacting_upgrade', count: 1 }, consumeInstantly: true },
+              { type: 'masterfulmachinery:items', data: { item: 'compactmachines:machine_small', count: 1 }, consumeInstantly: true },
               { type: 'masterfulmachinery:energy', perTick: true, data: { amount: 1000 } }
           ],
           ticks: 150,
@@ -40,10 +40,10 @@ onEvent('recipes', (event) => {
       {
           outputs: [{ type: 'masterfulmachinery:items', data: { item: 'compactmachines:machine_large', count: 1 } }],
           inputs: [
-              { type: 'masterfulmachinery:items', data: { item: 'compactmachines:wall', count: 48 } },
-              { type: 'masterfulmachinery:items', data: { item: 'thermal:upgrade_augment_1', count: 1 } },
-              { type: 'masterfulmachinery:items', data: { item: 'prefab:block_compressed_obsidian', count: 1 } },
-              { type: 'masterfulmachinery:items', data: { item: 'compactmachines:machine_normal', count: 1 } },
+              { type: 'masterfulmachinery:items', data: { item: 'compactmachines:wall', count: 48 }, consumeInstantly: true },
+              { type: 'masterfulmachinery:items', data: { item: 'thermal:upgrade_augment_1', count: 1 }, consumeInstantly: true },
+              { type: 'masterfulmachinery:items', data: { item: 'prefab:block_compressed_obsidian', count: 1 }, consumeInstantly: true },
+              { type: 'masterfulmachinery:items', data: { item: 'compactmachines:machine_normal', count: 1 }, consumeInstantly: true },
               { type: 'masterfulmachinery:energy', perTick: true, data: { amount: 1250 } }
           ],
           ticks: 150,
@@ -52,10 +52,10 @@ onEvent('recipes', (event) => {
       {
           outputs: [{ type: 'masterfulmachinery:items', data: { item: 'compactmachines:machine_giant', count: 1 } }],
           inputs: [
-              { type: 'masterfulmachinery:items', data: { item: 'compactmachines:wall', count: 64 } },
-              { type: 'masterfulmachinery:items', data: { item: 'thermal:upgrade_augment_2', count: 1 } },
-              { type: 'masterfulmachinery:items', data: { item: 'botania:mana_diamond_block', count: 1 } },
-              { type: 'masterfulmachinery:items', data: { item: 'compactmachines:machine_large', count: 1 } },
+              { type: 'masterfulmachinery:items', data: { item: 'compactmachines:wall', count: 64 }, consumeInstantly: true },
+              { type: 'masterfulmachinery:items', data: { item: 'thermal:upgrade_augment_2', count: 1 }, consumeInstantly: true },
+              { type: 'masterfulmachinery:items', data: { item: 'botania:mana_diamond_block', count: 1 }, consumeInstantly: true },
+              { type: 'masterfulmachinery:items', data: { item: 'compactmachines:machine_large', count: 1 }, consumeInstantly: true },
               { type: 'masterfulmachinery:energy', perTick: true, data: { amount: 1500 } }
           ],
           ticks: 150,
@@ -64,14 +64,24 @@ onEvent('recipes', (event) => {
       {
           outputs: [{ type: 'masterfulmachinery:items', data: { item: 'compactmachines:machine_maximum', count: 1 } }],
           inputs: [
-              { type: 'masterfulmachinery:items', data: { item: 'compactmachines:wall', count: 128 } },
-              { type: 'masterfulmachinery:items', data: { item: 'cagedmobs:star_infused_netherite_ingot', count: 8 } },
-              { type: 'masterfulmachinery:items', data: { item: 'compactmachines:machine_giant', count: 1 } },
+              { type: 'masterfulmachinery:items', data: { item: 'compactmachines:wall', count: 128 }, consumeInstantly: true },
+              { type: 'masterfulmachinery:items', data: { item: 'cagedmobs:star_infused_netherite_ingot', count: 8 }, consumeInstantly: true },
+              { type: 'masterfulmachinery:items', data: { item: 'compactmachines:machine_giant', count: 1 }, consumeInstantly: true },
               { type: 'masterfulmachinery:energy', perTick: true, data: { amount: 1750 } }
           ],
           ticks: 150,
           id: 'compactmachines:machine_maximum'
       },
+      {
+          outputs: [{ type: 'masterfulmachinery:items', data: { item: 'compactmachines:wall', count: 4 } }],
+          inputs: [
+              { type: 'masterfulmachinery:items', data: { item: 'minecraft:redstone', count: 8 }, consumeInstantly: true },
+              { type: 'masterfulmachinery:items', data: { item: 'pneumaticcraft:compressed_iron_block', count: 1 }, consumeInstantly: true },
+              { type: 'masterfulmachinery:energy', perTick: true, data: { amount: 10 } }
+          ],
+          ticks: 150,
+          id: `${id_prefix}wall`
+      }
     ];
 
     recipes.forEach((recipe) => {
