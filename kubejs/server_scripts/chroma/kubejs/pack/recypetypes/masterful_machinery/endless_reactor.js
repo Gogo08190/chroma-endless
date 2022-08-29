273afee63203fcb683155a186b18473c9b580e8f
Thebloodshadow <45766546+Thebloodshadow@users.noreply.github.com>
2022-08-29 14:39:32 +0200

    add : Recycling craft for thruster and cell

--- kubejs/server_scripts/chroma/kubejs/pack/recypetypes/masterful_machinery/endless_reactor.js
+++ kubejs/server_scripts/chroma/kubejs/pack/recypetypes/masterful_machinery/endless_reactor.js
@@ -20,6 +20,38 @@ onEvent('recipes', (event) => {
           ],
           ticks: 512,
           id: `${id_prefix}low_energy_generation`
+      },
+      {
+          outputs: [
+            { type: 'masterfulmachinery:items', data: { item: 'powah:crystal_nitro', count: 1 } },
+            {
+              perTick: true,
+              type: 'masterfulmachinery:energy', data: { amount: 60000 }
+            }
+          ],
+          inputs: [
+              {
+                consumeInstantly: true,
+                type: 'masterfulmachinery:items', data: { item: "ironjetpacks:nitro_cell", count: 1 } }
+          ],
+          ticks: 512,
+          id: `${id_prefix}nitro_cell_recycling`
+      },
+      {
+          outputs: [
+            { type: 'masterfulmachinery:items', data: { item: 'powah:crystal_nitro', count: 1 } },
+            {
+              perTick: true,
+              type: 'masterfulmachinery:energy', data: { amount: 60000 }
+            }
+          ],
+          inputs: [
+              {
+                consumeInstantly: true,
+                type: 'masterfulmachinery:items', data: { item: "ironjetpacks:nitro_thruster", count: 1 } }
+          ],
+          ticks: 512,
+          id: `${id_prefix}nitro_thruster_recycling`
       }
     ];
 
