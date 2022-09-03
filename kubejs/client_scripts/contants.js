9f40ae18dd57ff62ac9f847f694320223e24d4a6
Gogo08 <gogo08190@gmail.com>
2022-09-03 10:02:24 +0200

    Remove furnace tier up upgrade + crystal furnace

--- kubejs/client_scripts/contants.js
+++ kubejs/client_scripts/contants.js
@@ -422,7 +422,28 @@ const itemsToHide = [
     'mob_grinding_utils:mob_swab_used',
 
     'tardis:cinnabar',
-    'tardis:cinnabar_ore'
+    'tardis:cinnabar_ore',
+
+    'ironfurnaces:crystal_furnace',
+    'ironfurnaces:million_furnace',
+    'ironfurnaces:rainbow_core',
+    'ironfurnaces:rainbow_plating',
+    'ironfurnaces:upgrade_allthemodium',
+    'ironfurnaces:upgrade_vibranium',
+    'ironfurnaces:upgrade_unobtainium',
+    'ironfurnaces:upgrade_iron',
+    'ironfurnaces:upgrade_gold',
+    'ironfurnaces:upgrade_diamond',
+    'ironfurnaces:upgrade_gold2',
+    'ironfurnaces:upgrade_iron2',
+    'ironfurnaces:upgrade_obsidian2',
+    'ironfurnaces:upgrade_silver',
+    'ironfurnaces:upgrade_copper',
+    'ironfurnaces:upgrade_copper',
+    'ironfurnaces:upgrade_crystal',
+    'ironfurnaces:upgrade_obsidian',
+    'ironfurnaces:upgrade_emerald',
+    'ironfurnaces:upgrade_silver2'
 ];
 
 let ee_types = ['clump', 'crystal', 'dirty_dust', 'shard', 'fragment', 'gravel'];
---
+++ kubejs/server_scripts/chroma/kubejs/pack/recypetypes/iron_furnaces/remove.js
@@ -0,0 +1,38 @@
+// priority: 100
+
+onEvent('recipes', (event) => {
+    const idRemovals = [
+
+    ];
+
+    const outputRemovals = [
+      'ironfurnaces:crystal_furnace',
+      'ironfurnaces:million_furnace',
+      'ironfurnaces:rainbow_core',
+      'ironfurnaces:rainbow_plating',
+      'ironfurnaces:upgrade_allthemodium',
+      'ironfurnaces:upgrade_vibranium',
+      'ironfurnaces:upgrade_unobtainium',
+      'ironfurnaces:upgrade_iron',
+      'ironfurnaces:upgrade_gold',
+      'ironfurnaces:upgrade_diamond',
+      'ironfurnaces:upgrade_gold2',
+      'ironfurnaces:upgrade_iron2',
+      'ironfurnaces:upgrade_obsidian2',
+      'ironfurnaces:upgrade_silver',
+      'ironfurnaces:upgrade_copper',
+      'ironfurnaces:upgrade_copper',
+      'ironfurnaces:upgrade_crystal',
+      'ironfurnaces:upgrade_obsidian',
+      'ironfurnaces:upgrade_emerald',
+      'ironfurnaces:upgrade_silver2'
+    ];
+
+    idRemovals.forEach((id) => {
+        event.remove({ id: id });
+    });
+
+    outputRemovals.forEach((output) => {
+        event.remove({ output: output });
+    });
+});
