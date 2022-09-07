092ed7417ed455894ec4a01c5129f994c7e2bfdf
Gogo08 <gogo08190@gmail.com>
2022-09-07 07:33:31 +0200

    Fix Custom Machinery item slots ⛔

--- kubejs/server_scripts/chroma/kubejs/pack/recypetypes/custommachinery/slice_n_splice.js
+++ kubejs/server_scripts/chroma/kubejs/pack/recypetypes/custommachinery/slice_n_splice.js
@@ -3,10 +3,10 @@ onEvent('recipes', event => {
 /* Zombie Electrode */
 
 event.recipes.custommachinery.custom_machine("custommachinery:slice_n_splice", 640)
-.requireItem('2x emendatusenigmatica:silicon_gem')
-.requireItem('2x enderioalloys:item_alloy_ingot_energetic_alloy')
-.requireItem('kubejs:uranium_capacitor')
-.requireItem('minecraft:zombie_head')
+.requireItem(Item.of('emendatusenigmatica:silicon_gem', 2), "input1")
+.requireItem(Item.of('enderioalloys:item_alloy_ingot_energetic_alloy', 2), "input2")
+.requireItem('kubejs:uranium_capacitor', "input3")
+.requireItem('minecraft:zombie_head', "input4")
 .requireFluidPerTick(Fluid.of('industrialforegoing:essence', 1))
 .requireEnergyPerTick(1240)
 .produceItem(Item.of('enderioalloys:item_material_skull_zombie_electrode', 1))
@@ -14,10 +14,10 @@ event.recipes.custommachinery.custom_machine("custommachinery:slice_n_splice", 6
 /* Z-Logic Controller */
 
 event.recipes.custommachinery.custom_machine("custommachinery:slice_n_splice", 840)
-.requireItem('2x enderioalloys:item_alloy_ingot_soularium')
-.requireItem('2x emendatusenigmatica:silicon_gem')
-.requireItem('enderioalloys:item_alloy_ingot_redstone_alloy')
-.requireItem('minecraft:zombie_head')
+.requireItem(Item.of('emendatusenigmatica:silicon_gem', 2), "input1")
+.requireItem(Item.of('enderioalloys:item_alloy_ingot_soularium', 2), "input2")
+.requireItem('enderioalloys:item_alloy_ingot_redstone_alloy', "input3")
+.requireItem('minecraft:zombie_head', "input4")
 .requireFluidPerTick(Fluid.of('industrialforegoing:essence', 1))
 .requireEnergyPerTick(1550)
 .produceItem(Item.of('enderioalloys:item_material_skull_zombie_controller', 1))
@@ -25,10 +25,10 @@ event.recipes.custommachinery.custom_machine("custommachinery:slice_n_splice", 8
 /* Skeletal Contractor */
 
 event.recipes.custommachinery.custom_machine("custommachinery:slice_n_splice", 840)
-.requireItem('2x enderioalloys:item_alloy_ingot_soularium')
-.requireItem('emendatusenigmatica:silicon_gem')
-.requireItem('minecraft:skeleton_skull')
-.requireItem('2x architects_palette:rotten_flesh_block')
+.requireItem('emendatusenigmatica:silicon_gem', "input1")
+.requireItem(Item.of('enderioalloys:item_alloy_ingot_soularium', 2), "input2")
+.requireItem(Item.of('architects_palette:rotten_flesh_block', 2), "input3")
+.requireItem('minecraft:skeleton_skull', "input4")
 .requireFluidPerTick(Fluid.of('industrialforegoing:essence', 1))
 .requireEnergyPerTick(1350)
 .produceItem(Item.of('enderioalloys:item_material_skull_skeletal_contractor', 1))
@@ -36,10 +36,10 @@ event.recipes.custommachinery.custom_machine("custommachinery:slice_n_splice", 8
 /* Ender Resonator */
 
 event.recipes.custommachinery.custom_machine("custommachinery:slice_n_splice", 840)
-.requireItem('2x enderioalloys:item_alloy_ingot_soularium')
-.requireItem('2x emendatusenigmatica:silicon_gem')
-.requireItem('enderioalloys:item_alloy_ingot_vibrant_alloy')
-.requireItem('tconstruct:enderman_head')
+.requireItem(Item.of('emendatusenigmatica:silicon_gem', 2), "input1")
+.requireItem(Item.of('enderioalloys:item_alloy_ingot_soularium', 2), "input2")
+.requireItem('enderioalloys:item_alloy_ingot_vibrant_alloy', "input3")
+.requireItem('tconstruct:enderman_head', "input4")
 .requireFluidPerTick(Fluid.of('industrialforegoing:essence', 1))
 .requireEnergyPerTick(2000)
 .produceItem(Item.of('enderioalloys:item_material_skull_ender_resonator', 1))
@@ -47,10 +47,10 @@ event.recipes.custommachinery.custom_machine("custommachinery:slice_n_splice", 8
 /* Binding Card */
 
 event.recipes.custommachinery.custom_machine("custommachinery:slice_n_splice", 840)
-.requireItem('2x enderioalloys:item_alloy_ingot_soularium')
-.requireItem('2x mekanism:alloy_infused')
-.requireItem('powah:blank_card')
-.requireItem('powah:ender_core')
+.requireItem(Item.of('mekanism:alloy_infused', 2), "input1")
+.requireItem(Item.of('enderioalloys:item_alloy_ingot_soularium', 2), "input2")
+.requireItem('powah:blank_card', "input3")
+.requireItem('powah:ender_core', "input4")
 .requireFluidPerTick(Fluid.of('industrialforegoing:essence', 1))
 .requireEnergyPerTick(2000)
 .produceItem(Item.of('powah:binding_card', 1))
