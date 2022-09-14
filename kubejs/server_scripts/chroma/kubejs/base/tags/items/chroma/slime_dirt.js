31fc088dc1692f3a07dcd959ffef966b8ba74b02
Z4RM <cloviswakfu@gmail.com>
2022-09-14 15:30:26 +0200

    Delete customs tags (replaced by OR Filters for quests or useless)

--- kubejs/server_scripts/chroma/kubejs/base/tags/items/chroma/slime_dirt.js
+++
@@ -1,8 +0,0 @@
-onEvent('item.tags', (event) => {
-    event.get('chroma:slime_dirt').add([
-            'tconstruct:earth_slime_dirt',
-            'tconstruct:sky_slime_dirt',
-            'tconstruct:ichor_slime_dirt',
-            'tconstruct:ender_slime_dirt'
-        ]);
-});
