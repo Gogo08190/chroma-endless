d896804408a5e6a72dbedc5e5848a16bb9ca14db
Gogo08 <gogo08190@gmail.com>
2022-09-16 07:14:46 +0200

    Removing undergarden catalyst smelting

---
+++ kubejs/server_scripts/chroma/kubejs/pack/recypetypes/undergarden/remove.js
@@ -0,0 +1,20 @@
+// priority: 100
+
+onEvent('recipes', (event) => {
+    const idRemovals = [
+      'undergarden:smelt_catalyst',
+
+    ];
+
+    const outputRemovals = [
+
+    ];
+
+    idRemovals.forEach((id) => {
+        event.remove({ id: id });
+    });
+
+    outputRemovals.forEach((output) => {
+        event.remove({ output: output });
+    });
+});
