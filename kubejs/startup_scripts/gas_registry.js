6547d57920b2f6c2665a21ec369f4bdeab12db6c
YukiiSamaa <thomas.levasseur.tl@gmail.com>
2022-10-06 20:33:53 +0200

    Test add gas custom

---
+++ kubejs/startup_scripts/gas_registry.js
@@ -0,0 +1,13 @@
+onEvent('gas.registry', (event) => {
+    const generalGas = [
+      {
+        id: 'black',
+        color: 000000,
+        texture: 'kubejs:gas/base_gas_texture',
+        display: 'Black'
+      },
+      ];
+      generalGas.forEach((gas) => {
+          event.create(gas.id).color(gas.color).texture(gas.texture).displayName(gas.display);
+      });
+  });
