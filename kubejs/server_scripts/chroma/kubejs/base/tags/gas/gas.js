490ddf927ca1bad88e937d50c1919940780a7893
YukiiSamaa <thomas.levasseur.tl@gmail.com>
2022-10-08 19:40:05 +0200

    Add fist gas

--- kubejs/server_scripts/chroma/kubejs/base/tags/gas/gas.js
+++ kubejs/server_scripts/chroma/kubejs/base/tags/gas/gas.js
@@ -1,5 +1,12 @@
 onEvent('gas.tags', (event) => {
 
     event.get('kubejs:black').add(['kubejs:black']);
+    event.get('kubejs:light').add(['kubejs:light']);
+    event.get('kubejs:green').add(['kubejs:green']);
+    event.get('kubejs:pink').add(['kubejs:pink']);
+    event.get('kubejs:red').add(['kubejs:red']);
+    event.get('kubejs:blue').add(['kubejs:blue']);
+    event.get('kubejs:orange').add(['kubejs:orange']);
+    event.get('kubejs:purple').add(['kubejs:purple']);
 
 });
--- kubejs/startup_scripts/gas_registry.js
+++ kubejs/startup_scripts/gas_registry.js
@@ -1,10 +1,60 @@
 onEvent('gas.registry', (event) => {
     const generalGas = [
+      //Black
       {
         id: 'black',
         color: 000000,
         texture: 'kubejs:gas/base_gas_texture',
         display: 'Black Gas'
+      },
+      //Light
+      {
+        id: 'light',
+        color: 16777215,
+        texture: 'kubejs:gas/base_gas_texture',
+        display: 'Light Gas'
+      },
+      //Green
+      {
+        id: 'green',
+        color: 2486811,
+        texture: 'kubejs:gas/base_gas_texture',
+        display: 'Green Gas'
+      },
+      //Pink
+      {
+        id: 'pink',
+        color: 16711854,
+        texture: 'kubejs:gas/base_gas_texture',
+        display: 'Pink Gas'
+      },
+      //Red
+      {
+        id: 'red',
+        color: 16711684,
+        texture: 'kubejs:gas/base_gas_texture',
+        display: 'Red Gas'
+      },
+      //blue
+      {
+        id: 'blue',
+        color: 2752767,
+        texture: 'kubejs:gas/base_gas_texture',
+        display: 'Blue Gas'
+      },
+      //orange
+      {
+        id: 'orange',
+        color: 15744019,
+        texture: 'kubejs:gas/base_gas_texture',
+        display: 'Orange Gas'
+      },
+      //purple
+      {
+        id: 'purple',
+        color: 13048818,
+        texture: 'kubejs:gas/base_gas_texture',
+        display: 'Purple Gas'
       },
       ];
       generalGas.forEach((gas) => {
