6ce110f08e80ddb219d3e89923f88d00ef121a89
Gogo08 <gogo08190@gmail.com>
2022-10-16 16:51:01 +0200

    automatic compound recipes

--- kubejs/server_scripts/chroma/kubejs/pack/recypetypes/masterful_machinery/auto_infuser.js
+++ kubejs/server_scripts/chroma/kubejs/pack/recypetypes/masterful_machinery/auto_infuser.js
@@ -4,15 +4,26 @@ onEvent('recipes', (event) => {
       {
           outputs: [{ type: 'masterfulmachinery:items', data: { item: 'create:refined_radiance', count: 1 } }],
           inputs: [
-              { type: 'masterfulmachinery:items', data: { item: 'create:chromatic_compound', count: 1 } },
-              { type: 'masterfulmachinery:items', data: { item: 'thermal:lightning_charge', count: 1 } },
+              { type: 'masterfulmachinery:items', data: { item: 'create:chromatic_compound'', count: 1 } },
               { type: 'masterfulmachinery:items', data: { item: 'tconstruct:glow_ball', count: 1 } },
-              { type: 'masterfulmachinery:items', data: { item: 'torchmaster:megatorch', count: 1 } },
-              { type: 'masterfulmachinery:fluids', data: { fluid: 'thermal:glowstone', amount: 250 } },
-              { type: 'masterfulmachinery:fluids', data: { fluid: 'tconstruct:molten_debris', amount: 250 } }
+              { type: 'masterfulmachinery:items', data: { item: 'thermal:lightning_charge', count: 1 } },
+              { type: 'masterfulmachinery:fluids', data: { fluid: 'tconstruct:blazing_blood', amount: 250 } },
+              { type: 'masterfulmachinery:fluids', data: { fluid: 'tconstruct:molten_obsidian', amount: 250 } }
           ],
           ticks: 240,
           id: `${id_prefix}refined_radiance`
+      },
+      {
+          outputs: [{ type: 'masterfulmachinery:items', data: { item: 'create:shadow_steel', count: 1 } }],
+          inputs: [
+              { type: 'masterfulmachinery:items', data: { item: 'create:chromatic_compound'', count: 1 } },
+              { type: 'masterfulmachinery:items', data: { item: 'occultism:spirit_attuned_crystal', count: 1 } },
+              { type: 'masterfulmachinery:items', data: { item: 'occultism:otherstone', count: 1 } },
+              { type: 'masterfulmachinery:fluids', data: { fluid: 'tconstruct:blazing_blood', amount: 250 } },
+              { type: 'masterfulmachinery:fluids', data: { fluid: 'tconstruct:molten_obsidian', amount: 250 } }
+          ],
+          ticks: 240,
+          id: `${id_prefix}shadow_steel`
       },
     ];
 
