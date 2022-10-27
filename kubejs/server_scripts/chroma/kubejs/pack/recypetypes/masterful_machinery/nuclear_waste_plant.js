d249dd9f2c167cf8910e148c6f40c1b2ed7d6928
YukiiSamaa <thomas.levasseur.tl@gmail.com>
2022-10-27 14:37:30 +0200

    Add craft black gas

--- kubejs/server_scripts/chroma/kubejs/pack/recypetypes/masterful_machinery/nuclear_waste_plant.js
+++ kubejs/server_scripts/chroma/kubejs/pack/recypetypes/masterful_machinery/nuclear_waste_plant.js
@@ -50,6 +50,32 @@ onEvent('recipes', (event) => {
           ],
           ticks: 512,
           id: `${id_prefix}waste_recycling_bis`
+      },
+      //Craft Black Gas
+      {
+          outputs: [
+            { type: 'masterfulmachinery:mekanism_gas', data: { gas: 'mekanism:black', amount: 250 } }
+          ],
+          inputs: [
+              {
+                consumeInstantly: true,
+                type: 'masterfulmachinery:mekanism_gas', data: { gas: 'mekanism:spent_nuclear_waste', amount: 1000 }
+              },
+              {
+                consumeInstantly: true,
+                type: 'masterfulmachinery:fluids', data: { fluid: 'mekanism:heavy_water', amount: 4000 }
+              },
+              {
+                consumeInstantly: true,
+                type: 'masterfulmachinery:items', data: { item: 'kubejs:dark_ingot' , count: 8 }
+              },
+              {
+                consumeInstantly: true,
+                type: 'masterfulmachinery:energy', data: { amount: 250000 }
+              }
+          ],
+          ticks: 512,
+          id: `${id_prefix}black_gas`
       }
     ];
 
