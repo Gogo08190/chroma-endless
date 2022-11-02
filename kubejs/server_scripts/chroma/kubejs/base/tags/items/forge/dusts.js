ac18ce0475b09a71ba2d49e329610ce5d1b79c68
Gogo08 <gogo08190@gmail.com>
2022-11-02 07:11:07 +0100

    Fix unification for arcane (Ars nouveau)

--- kubejs/server_scripts/chroma/kubejs/base/tags/items/forge/dusts.js
+++ kubejs/server_scripts/chroma/kubejs/base/tags/items/forge/dusts.js
@@ -23,4 +23,18 @@ onEvent('item.tags', (event) => {
     event.add('forge:dusts/flour', 'create:wheat_flour');
     event.add('forge:dusts/flour', 'pedestals:dustflour');
 
+    event.get('forge:chunks/arcane').remove('emendatusenigmatica:arcane_chunk');
+    event.get('forge:ores/arcane').remove('emendatusenigmatica:arcane_chunk');
+    event.get('forge:ores/arcane').remove('emendatusenigmatica:arcane_ore');
+    event.get('forge:ores/arcane').remove('ars_nouveau:arcane_ore');
+    event.get('forge:ores/mana_gem').remove('ars_nouveau:arcane_ore');
+    event.get('forge:gems/arcane').remove('emendatusenigmatica:arcane_gem');
+    event.get('forge:storage_blocks/arcane').remove('emendatusenigmatica:arcane_block');
+    event.get('forge:storage_blocks/mana').add('emendatusenigmatica:arcane_block');
+    event.get('forge:chunks/mana').add('emendatusenigmatica:arcane_chunk');
+    event.get('forge:ores/mana').add('emendatusenigmatica:arcane_ore');
+    event.get('forge:ores/mana').add('emendatusenigmatica:arcane_chunk');
+    event.get('forge:gems/mana').add('emendatusenigmatica:arcane_gem');
+    event.get('forge:ores/mana').add('ars_nouveau:arcane_ore');
+
 });
