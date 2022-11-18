3bf546b90156f35b3a2fbe921f4d104deee8fa21
Gogo08 <gogo08190@gmail.com>
2022-11-18 17:48:55 +0100

    Fis

--- kubejs/server_scripts/chroma/kubejs/pack/recypetypes/tconstruct/alloy.js
+++ kubejs/server_scripts/chroma/kubejs/pack/recypetypes/tconstruct/alloy.js
@@ -4,14 +4,14 @@ onEvent('recipes', (event) => {
         {
             inputs: [
                 { name: 'emendatusenigmatica:molten_lapis', amount: 1440 },
-                { name: 'kubejs:molten_ionite', amount: 1000 },
-                { name: 'resourcefulbees:xion_honey', amount: 500 },
+                { name: 'kubejs:molten_ionite', amount: 1008 },
+                { name: 'resourcefulbees:xion_honey', amount: 1000 },
                 { name: 'kubejs:molten_mana_diamond', amount: 1152 },
                 { name: 'tconstruct:molten_brass', amount: 576 },
                 { name: 'tconstruct:molten_steel', amount: 576 },
                 { name: 'kubejs:molten_nether_star', amount: 288 }
             ],
-            result: { fluid: 'kubejs:mesophase', amount: 1000 },
+            result: { fluid: 'kubejs:mesophase', amount: 3000 },
             temperature: 1500,
             id: `${id_prefix}mesophase`
         }
