bfc73c75512799c6f2a745befda463676890eee1
Gogo08 <gogo08190@gmail.com>
2022-11-29 21:08:39 +0100

    [❕] Fix backpack

--- kubejs/server_scripts/chroma/kubejs/pack/recypetypes/sophisticatedbackpacks/shaped.js
+++ kubejs/server_scripts/chroma/kubejs/pack/recypetypes/sophisticatedbackpacks/shaped.js
@@ -1,42 +1,6 @@
 onEvent('recipes', (event) => {
   const recipes = [
     {
-      output: 'sophisticatedbackpacks:iron_backpack',
-      pattern: ['PPP', 'PBP', 'PPP'],
-      key: {
-        P: '#forge:plates/iron',
-        B: 'sophisticatedbackpacks:backpack'
-      },
-      id: 'sophisticatedbackpacks:iron_backpack'
-    },
-    {
-      output: 'sophisticatedbackpacks:gold_backpack',
-      pattern: ['PPP', 'PBP', 'PPP'],
-      key: {
-        P: '#forge:plates/gold',
-        B: 'sophisticatedbackpacks:iron_backpack'
-      },
-      id: 'sophisticatedbackpacks:gold_backpack'
-    },
-    {
-      output: 'sophisticatedbackpacks:diamond_backpack',
-      pattern: ['PPP', 'PBP', 'PPP'],
-      key: {
-        P: '#forge:plates/diamond',
-        B: 'sophisticatedbackpacks:gold_backpack'
-      },
-      id: 'sophisticatedbackpacks:diamond_backpack'
-    },
-    {
-      output: 'sophisticatedbackpacks:netherite_backpack',
-      pattern: ['PPP', 'PBP', 'PPP'],
-      key: {
-        P: '#forge:plates/netherite',
-        B: 'sophisticatedbackpacks:diamond_backpack'
-      },
-      id: 'sophisticatedbackpacks:netherite_backpack'
-    },
-    {
       output: 'sophisticatedbackpacks:upgrade_base',
       pattern: ['SPS', 'PLP', 'SPS'],
       key: {
