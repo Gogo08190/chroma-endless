5ac3da3d32413cb4ff298a1b7593b9147a4efb51
koepss <pierrelode2@outlook.fr>
2022-11-30 20:19:43 +0100

    delete honey tank

--- kubejs/server_scripts/chroma/kubejs/pack/recypetypes/resourcefullbees/remove.js
+++ kubejs/server_scripts/chroma/kubejs/pack/recypetypes/resourcefullbees/remove.js
@@ -2,7 +2,10 @@
 
 onEvent('recipes', (event) => {
     const idRemovals = [
-      'creativeapiary:creative_apiary_breeder'
+      'creativeapiary:creative_apiary_breeder',
+      'resourcefulbees:wooden_honey_tank',
+      'resourcefulbees:nether_honey_tank',
+      'resourcefulbees:purpur_honey_tank'
     ];
 
     const outputRemovals = [
