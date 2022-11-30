c32af9dea1e6293f2cde37aad5d9e06fb14839ac
Hugo <gogo08190@gmail.com>
2022-11-30 14:42:00 +0100

    [❕] Fix dank null clear after upgrade

--- kubejs/server_scripts/chroma/kubejs/pack/recypetypes/dank-tanknull/dankstorage/shaped.js
+++ kubejs/server_scripts/chroma/kubejs/pack/recypetypes/dank-tanknull/dankstorage/shaped.js
@@ -19,65 +19,6 @@ onEvent('recipes', (event) => {
       id: 'dankstorage:1'
     },
     {
-      output: 'dankstorage:dank_2',
-      pattern: ['ABA', 'BCB', 'ABA'],
-      key: {
-        A: 'thermal:hazmat_fabric',
-        B: 'extendedcrafting:redstone_ingot_block',
-        C: Item.of('dankstorage:dank_1').ignoreNBT()
-      },
-      id: 'dankstorage:2'
-    },
-    {
-      output: 'dankstorage:dank_3',
-      pattern: ['AAA', 'BCB', 'AAA'],
-      key: {
-        A: '#forge:storage_blocks/electrum',
-        B: 'thermal:upgrade_augment_2',
-        C: Item.of('dankstorage:dank_2').ignoreNBT()
-      },
-      id: 'dankstorage:3'
-    },
-    {
-      output: 'dankstorage:dank_4',
-      pattern: ['AAA', 'ABA', 'AAA'],
-      key: {
-        A: 'botanicalmachinery:mana_emerald_block',
-        B: Item.of('dankstorage:dank_3').ignoreNBT()
-      },
-      id: 'dankstorage:4'
-    },
-    {
-      output: 'dankstorage:dank_5',
-      pattern: ['ABA', 'CDC', 'ABA'],
-      key: {
-        A: '#forge:storage_blocks/drenched_iron',
-        B: '#forge:ingots/fireite',
-        C: '#forge:storage_blocks/swift_alloy',
-        D: Item.of('dankstorage:dank_4').ignoreNBT()
-      },
-      id: 'dankstorage:5'
-    },
-    {
-      output: 'dankstorage:dank_6',
-      pattern: ['ABA', 'BCB', 'ABA'],
-      key: {
-        A: 'bigreactors:reinforced_reactorcasing',
-        B: 'bigreactors:fluidizercasing',
-        C: Item.of('dankstorage:dank_5').ignoreNBT()
-      },
-      id: 'dankstorage:6'
-    },
-    {
-      output: 'dankstorage:dank_7',
-      pattern: ['AAA', 'ABA', 'AAA'],
-      key: {
-        A: 'extendedcrafting:ender_star',
-        B: Item.of('dankstorage:dank_6').ignoreNBT()
-      },
-      id: 'dankstorage:7'
-    },
-    {
       output: 'dankstorage:1_to_2',
       pattern: ['ABA', 'BCB', 'ABA'],
       key: {
