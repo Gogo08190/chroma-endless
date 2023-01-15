0b013df078347678446ae18d7f4ba56fa4427154
Gogo08 <gogo08190@gmail.com>
2023-01-15 18:30:04 +0100

    [💫] Add revert craft block to ingot Ender IO + fix

--- kubejs/server_scripts/chroma/kubejs/pack/recypetypes/enderioalloys/shapeless.js
+++ kubejs/server_scripts/chroma/kubejs/pack/recypetypes/enderioalloys/shapeless.js
@@ -126,6 +126,14 @@ onEvent('recipes', (event) => {
             ],
             id: `${id_prefix}block_alloy_melodic_alloy`
         },
+
+        {
+            output: 'enderioalloys:block_alloy_vivid_alloy',
+            inputs: [
+                '9x enderioalloys:item_alloy_ingot_vivid_alloy'
+            ],
+            id: `${id_prefix}block_alloy_vivid_alloy`
+        },
         {
             output: '9x enderioalloys:item_alloy_nugget_conductive_iron',
             inputs: ['enderioalloys:item_alloy_ingot_conductive_iron'],
@@ -263,6 +271,16 @@ onEvent('recipes', (event) => {
             id: `${id_prefix}item_alloy_ingot_soularium`
         },
         {
+            output: 'enderioalloys:item_alloy_ingot_crude_steel',
+            inputs: ['9x enderioalloys:item_alloy_nugget_crude_steel'],
+            id: `${id_prefix}item_alloy_ingot_crude_steel`
+        },
+        {
+            output: 'enderioalloys:item_alloy_ingot_pulsating_iron',
+            inputs: ['9x enderioalloys:item_alloy_nugget_pulsating_iron'],
+            id: `${id_prefix}item_alloy_nugget_pulsating_iron_steel`
+        },
+        {
             output: 'enderioalloys:item_alloy_ingot_stellar_alloy',
             inputs: ['9x enderioalloys:item_alloy_nugget_stellar_alloy'],
             id: `${id_prefix}item_alloy_ingot_stellar_alloy`
@@ -276,6 +294,86 @@ onEvent('recipes', (event) => {
             output: 'enderioalloys:item_alloy_ingot_vivid_alloy',
             inputs: ['9x enderioalloys:item_alloy_nugget_vivid_alloy'],
             id: `${id_prefix}item_alloy_ingot_vivid_alloy`
+        },
+        {
+            output: '9x enderioalloys:item_alloy_ingot_vivid_alloy',
+            inputs: ['enderioalloys:block_alloy_vivid_alloy'],
+            id: `${id_prefix}block_alloy_vivid_alloy_to_ingot`
+        },
+        {
+            output: '9x enderioalloys:item_alloy_ingot_vibrant_alloy',
+            inputs: ['enderioalloys:block_alloy_vibrant_alloy'],
+            id: `${id_prefix}block_alloy_vibrant_alloy_to_ingot`
+        },
+        {
+            output: '9x enderioalloys:item_alloy_ingot_stellar_alloy',
+            inputs: ['enderioalloys:block_alloy_stellar_alloy'],
+            id: `${id_prefix}block_alloy_stellar_alloy_to_ingot`
+        },
+        {
+            output: '9x enderioalloys:item_alloy_ingot_soularium',
+            inputs: ['enderioalloys:block_alloy_soularium'],
+            id: `${id_prefix}block_alloy_soularium_to_ingot`
+        },
+        {
+            output: '9x enderioalloys:item_alloy_ingot_redstone_alloy',
+            inputs: ['enderioalloys:block_alloy_redstone_alloy'],
+            id: `${id_prefix}block_alloy_redstone_alloy_to_ingot`
+        },
+        {
+            output: '9x enderioalloys:item_alloy_ingot_pulsating_iron',
+            inputs: ['enderioalloys:block_alloy_pulsating_iron'],
+            id: `${id_prefix}block_alloy_pulsating_iron_to_ingot`
+        },
+        {
+            output: '9x enderioalloys:item_alloy_ingot_melodic_alloy',
+            inputs: ['enderioalloys:block_alloy_melodic_alloy'],
+            id: `${id_prefix}block_alloy_melodic_alloy_to_ingot`
+        },
+        {
+            output: '9x enderioalloys:item_alloy_ingot_energetic_silver',
+            inputs: ['enderioalloys:block_alloy_energetic_silver'],
+            id: `${id_prefix}block_alloy_energetic_silver_to_ingot`
+        },
+        {
+            output: '9x enderioalloys:item_alloy_ingot_energetic_alloy',
+            inputs: ['enderioalloys:block_alloy_energetic_alloy'],
+            id: `${id_prefix}block_alloy_energetic_alloy_to_ingot`
+        },
+        {
+            output: '9x enderioalloys:item_alloy_ingot_end_steel',
+            inputs: ['enderioalloys:block_alloy_end_steel'],
+            id: `${id_prefix}block_alloy_end_steel_to_ingot`
+        },
+        {
+            output: '9x enderioalloys:item_alloy_ingot_electrical_steel',
+            inputs: ['enderioalloys:block_alloy_electrical_steel'],
+            id: `${id_prefix}block_alloy_electrical_steel_to_ingot`
+        },
+        {
+            output: '9x enderioalloys:item_alloy_ingot_dark_steel',
+            inputs: ['enderioalloys:block_alloy_dark_steel'],
+            id: `${id_prefix}block_alloy_dark_steel_to_ingot`
+        },
+        {
+            output: '9x enderioalloys:item_alloy_ingot_crystalline_pink_slime',
+            inputs: ['enderioalloys:block_alloy_crystalline_pink_slime'],
+            id: `${id_prefix}block_alloy_crystalline_pink_slime_to_ingot`
+        },
+        {
+            output: '9x enderioalloys:item_alloy_ingot_crystalline_alloy',
+            inputs: ['enderioalloys:block_alloy_crystalline_alloy'],
+            id: `${id_prefix}block_alloy_crystalline_alloy_to_ingot`
+        },
+        {
+            output: '9x enderioalloys:item_alloy_ingot_crude_steel',
+            inputs: ['enderioalloys:block_alloy_crude_steel'],
+            id: `${id_prefix}block_alloy_crude_steel_to_ingot`
+        },
+        {
+            output: '9x enderioalloys:item_alloy_ingot_conductive_iron',
+            inputs: ['enderioalloys:block_alloy_conductive_iron'],
+            id: `${id_prefix}block_alloy_conductive_iron_to_ingot`
         }
     ];
     recipes.forEach((recipe) => {
