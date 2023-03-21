9c8df483b88722795ab21d184d1b8ac385892504
Hugo <gogo08190@gmail.com>
2023-03-21 14:09:22 +0100

    [✨] Add draconium ore process create

--- kubejs/server_scripts/chroma/kubejs/pack/recypetypes/create/milling.js
+++ kubejs/server_scripts/chroma/kubejs/pack/recypetypes/create/milling.js
@@ -7,6 +7,12 @@ onEvent('recipes', (event) => {
             processingTime: 30,
             id: `${id_prefix}crying_obsidian_dust`
         },
+        {
+            input: '#forge:ores/draconium',
+            outputs: [Item.of('draconicevolution:draconium_dust', 4)],
+            processingTime: 30,
+            id: `${id_prefix}draconium_dust`
+        }
     ];
     recipes.forEach((recipe) => {
         event.recipes.create.milling(recipe.outputs, recipe.input).processingTime(recipe.processingTime).id(recipe.id);
