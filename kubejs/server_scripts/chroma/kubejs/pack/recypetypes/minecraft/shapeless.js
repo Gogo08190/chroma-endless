253e58ec480ea0cd2abdb162ab40a5facc706070
Gogo08 <gogo08190@gmail.com>
2023-03-22 07:12:36 +0100

    [✨] Add recipes byg foods

--- kubejs/server_scripts/chroma/kubejs/pack/recypetypes/minecraft/shapeless.js
+++ kubejs/server_scripts/chroma/kubejs/pack/recypetypes/minecraft/shapeless.js
@@ -29,6 +29,21 @@ onEvent('recipes', (event) => {
           output: 'byg:soul_shroom',
           inputs: ['#forge:mushrooms', 'nethers_delight:rich_soul_soil'],
           id: 'byg:soul_shroom'
+      },
+      {
+          output: 'byg:death_cap',
+          inputs: ['#forge:mushrooms', 'minecraft:rotten_flesh'],
+          id: 'byg:death_cap'
+      },
+      {
+          output: 'byg:nightshade_berries',
+          inputs: ['minecraft:sweet_berries', 'minecraft:warped_fungus'],
+          id: 'byg:nightshade_berries'
+      },
+      {
+          output: 'byg:crimson_berries',
+          inputs: ['minecraft:sweet_berries', 'minecraft:crimson_fungus'],
+          id: 'byg:crimson_berries'
       }
   	];
 
