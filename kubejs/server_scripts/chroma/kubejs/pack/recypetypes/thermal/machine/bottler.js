4efb7b781c43794bef4fabc7abea18d6915bbeef
Gogo08 <gogo08190@gmail.com>
2023-03-24 07:27:17 +0100

    [✨] Add ender ingot + eye of ender Thermal Bottler

--- kubejs/server_scripts/chroma/kubejs/pack/recypetypes/thermal/machine/bottler.js
+++ kubejs/server_scripts/chroma/kubejs/pack/recypetypes/thermal/machine/bottler.js
@@ -16,6 +16,20 @@ onEvent('recipes', (event) => {
           output: 'pneumaticcraft:reinforced_stone',
           energy: 8000,
           id: 'pneumaticcraft:reinforced_stone'
+      },
+      {
+          input: 'minecraft:ender_pearl',
+          fluid: Fluid.of('tconstruct:blazing_blood', 125),
+          output: 'minecraft:ender_eye',
+          energy: 1000,
+          id: `${id_prefix}ender_eye`
+      },
+      {
+          input: 'minecraft:iron_ingot',
+          fluid: Fluid.of('tconstruct:molten_ender', 125),
+          output: 'extendedcrafting:ender_ingot',
+          energy: 1000,
+          id: `${id_prefix}ender_ingot`
       }
     ];
     recipes.forEach((recipe) => {
